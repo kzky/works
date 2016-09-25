2252446b3b9d906360be35394138d262ee41ec3e
kzky <rkzfilter@gmail.com>
2016-09-25 17:39:41 +0900

    Add train one-step test.

--- graph_ssl/tests/test_models.py
+++ graph_ssl/tests/test_models.py
@@ -1,7 +1,8 @@
 """Models Tests
 """
 import numpy as np
-from models import MLP, CrossEntropy, RBF, GraphLoss, SSLGraphLoss
+from chainer import optimizers
+from models import MLP, CrossEntropy, RBF, GraphLoss, SSLGraphLoss, GraphSSLMLPModel
 
 def test_loss():
     batch_size = 32
@@ -9,26 +10,31 @@ def test_loss():
     out_dim = 10
     dims = [inp_dim, 1000, 500, 250, 250, 250, out_dim]
 
-    x_l = np.random.rand(batch_size, inp_dim).astype(np.float32)
+    x_l = np.random.randn(batch_size, inp_dim).astype(np.float32)
     y_l = np.random.uniform(0, out_dim, batch_size).astype(np.int32)
-    x_u_0 = np.random.rand(batch_size, inp_dim).astype(np.float32)
-    x_u_1 = np.random.rand(batch_size, inp_dim).astype(np.float32)
+    x_u_0 = np.random.randn(batch_size, inp_dim).astype(np.float32)
+    x_u_1 = np.random.randn(batch_size, inp_dim).astype(np.float32)
 
-    mlp_l = MLP(dims)
-    mlp_u_0 = mlp_l.copy()  # weight tying
-    mlp_u_1 = mlp_l.copy()  # weight tying
-    sloss = CrossEntropy(mlp_l)
-    gloss = GraphLoss(mlp_u_0, mlp_u_1, dims, batch_size)
-    model = SSLGraphLoss(sloss, gloss)
+    model = GraphSSLMLPModel(dims, batch_size)
+    fc0_W_data = model.mlp_l["fc0"].W.data.copy()  # once copy
+        
+    # Optimizer
+    optimizer = optimizers.SGD()
+    optimizer.setup(model)
 
-    # Forward
-    loss = model(x_l, y_l, x_u_0, x_u_1)
-
-    # Backward
-    loss.zerograd()
+    # Train one-step
+    model.zerograds()
+    loss = model.foward(x_l, y_l, x_u_0, x_u_1)
     loss.backward()
+    optimizer.update()
 
-    print(loss.data)
+    # Check grad computation
+    assert np.all(model.mlp_l["fc0"].W.grad != 0.)
+
+    # Check data update
+    assert np.any(fc0_W_data != model.mlp_l["fc0"].W.data)
+
+    # Check shared params
+    assert np.allclose(model.mlp_l["fc0"].W.data, model.mlp_u_0["fc0"].W.data)
     
-
     
