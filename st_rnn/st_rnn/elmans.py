97de2ad98a35288807fb61b980349e502df6a5ab
kzky <rkzfilter@gmail.com>
2016-10-16 19:51:58 +0900

    Add first sample.

---
+++ st_rnn/st_rnn/elmans.py
@@ -0,0 +1,46 @@
+"""Elamn Models
+"""
+
+def forward_backward_update_050(
+        rnn,
+        rnn_labeled_loss,
+        rnn_unlabeled_loss,
+        optimizer,
+        model, 
+        x_l, y_l, x_u):
+
+    T = rnn.T
+
+    # Forward/Backward/Update in labeled graph
+    model.cleargrads()
+    x_list = [x_l for _ in range(T)]
+    y_list = rnn(x_list)
+    l_losses = rnn_labeled_loss(y_list, y_l)
+    loss = reduce(lambda x, y: x + y, l_losses)
+    loss.backward()
+    optimizer.update()
+        
+    # Forward/Backward/Update in unlabeled graph
+    model.cleargrads()
+    x_list = [x_l for _ in range(T)]
+    y_list = rnn(x_list)
+    u_losses = rnn_unlabeled_loss(y_list)
+    loss = reduce(lambda x, y: x + y, u_losses)
+    loss.backward()
+    optimizer.update()
+    
+def evaluate_050(
+        rnn,
+        rnn_labeled_loss,
+        x_l, y_l):
+
+    T = rnn.T
+
+    # Forward/Backward/Update in labeled graph
+    x_list = [x_l for _ in range(T)]
+    y_list = rnn(x_list)
+    l_losses = rnn_labeled_loss(y_list, y_l)
+    return l_losses
+
+        
+    
