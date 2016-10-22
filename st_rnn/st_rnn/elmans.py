22bc07f2463918486e00bd5e7d0248bd4c5014b4
kzky <rkzfilter@gmail.com>
2016-10-22 20:25:53 +0900

    Add experiments 052 and 056.

--- st_rnn/st_rnn/elmans.py
+++ st_rnn/st_rnn/elmans.py
@@ -146,6 +146,54 @@ def evaluate_054(
     l_losses = rnn_labeled_losses(y_list, y_l)
     return l_losses
 
+"""
+056
+"""
+def forward_backward_update_056(
+        rnn,
+        rnn_labeled_losses,
+        rnn_unlabeled_losses,
+        optimizer,
+        model, 
+        x_l, y_l, x_u):
+
+    T = rnn.T
+
+    # Forward/Backward/Update in labeled graph
+    model.reset_states()
+    model.cleargrads()
+    x_list = [x_l for _ in range(T)]
+    y_list = rnn(x_list)
+    l_losses = rnn_labeled_losses(y_list, y_l)
+    loss = l_losses[-1]
+    loss.backward()
+    optimizer.update()
+        
+    # Forward/Backward/Update in unlabeled graph
+    model.reset_states()
+    model.cleargrads()
+    x_list = [x_l for _ in range(T)]
+    y_list = rnn(x_list)
+    u_losses = rnn_unlabeled_losses(y_list)
+    loss = u_losses[-1]
+    loss.backward()
+    optimizer.update()
+    
+def evaluate_056(
+        rnn,
+        rnn_labeled_losses,
+        model,
+        x_l, y_l):
+
+    T = rnn.T
+
+    # Forward/Backward/Update in labeled graph
+    model.reset_states()
+    x_list = [x_l for _ in range(T)]
+    y_list = rnn(x_list)
+    l_losses = rnn_labeled_losses(y_list, y_l)
+    return l_losses
+
 
 """
 062
