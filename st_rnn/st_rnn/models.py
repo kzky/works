9ddf58c073424759221e388d2075a64211c4a8ba
kzky <rkzfilter@gmail.com>
2016-10-30 19:16:45 +0900

    Add import.

--- st_rnn/st_rnn/models.py
+++ st_rnn/st_rnn/models.py
@@ -2,6 +2,7 @@
 """
 import numpy as np
 import chainer
+import chainer.variable as variable
 from chainer import cuda, Function, gradient_check, report, training, utils, Variable
 from chainer import datasets, iterators, optimizers, serializers
 from chainer import Link, Chain, ChainList
