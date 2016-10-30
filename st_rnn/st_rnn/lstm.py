958eacbbbfdfa7d07ef90609c13f2cfc49298bda
kzky <rkzfilter@gmail.com>
2016-10-30 16:50:37 +0900

    Add experiment mock with LSTM.

---
+++ st_rnn/st_rnn/lstm.py
@@ -0,0 +1,55 @@
+"""LSTM Models
+"""
+
+"""
+098
+"""
+def forward_backward_update_098(
+        rnn,
+        rnn_labeled_losses,
+        rnn_unlabeled_losses,
+        optimizer,
+        model, 
+        x_l, y_l, x_u):
+
+    T = rnn.T
+
+
+"""
+100
+"""
+def forward_backward_update_100(
+        rnn,
+        rnn_labeled_losses,
+        rnn_unlabeled_losses,
+        optimizer,
+        model, 
+        x_l, y_l, x_u):
+
+    T = rnn.T
+
+"""
+102
+"""
+def forward_backward_update_102(
+        rnn,
+        rnn_labeled_losses,
+        rnn_unlabeled_losses,
+        optimizer,
+        model, 
+        x_l, y_l, x_u):
+
+    T = rnn.T
+
+"""
+104
+"""
+def forward_backward_update_104(
+        rnn,
+        rnn_labeled_losses,
+        rnn_unlabeled_losses,
+        optimizer,
+        model, 
+        x_l, y_l, x_u):
+
+    T = rnn.T
