14cba0092bd664aa276326d4ce5a05ab69d547a6
kzky <rkzfilter@gmail.com>
2016-11-03 20:08:17 +0900

    Add some necessities.

---
+++ da_recon/da_recon/experiments.py
@@ -0,0 +1,86 @@
+from models import MLPEnc, MLPDec, SupervizedLoss, ReconstructionLoss
+from chainer import optimizers, Variable
+
+class Experiment000(object):
+    """Experiment takes responsibility for a batch not for train-loop.
+    """
+
+    def __init__(self,
+                     dims,
+                     act=F.Relu,
+                     lambdas = [1., 1., 1.]
+                     learning_rate=1. * 1e-2,
+                     bn=True,
+                     noise=False,
+                     lateral=False,
+                     test=False,
+                     device=None):
+
+        # Settting
+        self.lambdas = lambdas
+        self.T = len(lambdas)
+        
+        # Model
+        self.model = Model(
+            dims=dims, act=act,
+            bn=bn, noise=noise, lateral=lateral, test=test)
+        self.mlp_enc = self.model.mlp_enc
+        self.mlp_dec = self.model.mlp_dec
+        self.supervised_loss = self.model.supervised_loss
+        self.recon_loss = self.model.recon_loss
+
+        # Optimizer
+        self.optimizer = optimizers.Adam(learning_rate)
+        self.optimizer.setup(model)
+        self.oprimizer.use_cleargrads()
+        
+    def train(self, x_l, y_l, x_u, y_u):
+        # Forward
+        supervised_losses = []
+        recon_loss_ls = []
+        recon_loss_us = []
+
+        x_l_recon = x_l
+        x_u_recon = x_u
+        for t in range(T):
+            # Supervision for (x_l, y_l)
+            y = self.mlp_enc(x_l_recon)
+            supervised_loss = self.supervised_loss(y, y_l)
+            supervised_losses.append(supervised_loss)
+            
+            # Reconstruction for (x_l, y_l)
+            x_l_recon = self.mlp_dec(y)
+            recon_loss_l = self.recon_loss(x_l_recon, x_l,
+                                               self.mlp_enc.linears.values(),
+                                               self.mlp_dec.linears.values())
+            recon_loss_ls.append(recon_loss_l)
+
+            # Reconstruction for (x_u, y_u)
+            y = self.mlp_enc(x_u_recon)
+            x_u_recon = self.mlp_dec(y, enc_hiddens, dec_hiddens)
+            recon_loss_u = self.recon_loss(x_u_recon, x_u,
+                                               self.mlp_enc.linears.values(), 
+                                               self.mlp_dec.linears.values())        
+            recon_loss_us.append(recon_loss_u)
+
+        # Loss
+        supervised_loss = reduce(lambda x, y: x + y)
+        recon_loss_l = 0
+        recon_loss_u = 0
+        for lambda_, l0, l1 in zip(self.lambdas, recon_loss_ls, recon_loss_us):
+            recon_loss_l += lambda_ * l0
+            recon_loss_u += lambda_ * l1
+
+        loss = supervised_loss + recon_loss_l + recon_loss_u
+
+        # Backward
+        self.model.cleargrads()
+        loss.backward()
+
+        # Update
+        optimizer.update()
+
+    def test(self, x_l, y_l):
+        y = self.mlp_enc(x_l)
+        return F.accuracy(y, y_l)
+        
---
+++ da_recon/experiments/exp000.py
@@ -0,0 +1,7 @@
+from da_recon.models import ElmanOnestep, ElmanNet, RNNLabeledLosses, RNNUnlabeledLosses
+from da_recon.datasets import MNISTDataReader, Separator
+import numpy as np
+import os
+import numpy as np
+import sys
+import time
