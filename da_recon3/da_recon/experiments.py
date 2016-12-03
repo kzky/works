4ba26e1ec1c2ca370567eb1417a742373f9cdd23
kzky <rkzfilter@gmail.com>
2016-12-04 01:46:08 +0900

    Add more expeirments.

--- da_recon3/da_recon/experiments.py
+++ da_recon3/da_recon/experiments.py
@@ -115,102 +115,6 @@ class Experiment(object):
         recon_loss = losses[1]
         return acc, supervised_loss, recon_loss
         
-class Experiment070_3(Experiment):
-    """Experiment takes responsibility for a batch not for train-loop.
-    """
-
-    def __init__(self,
-                 device=None,
-                 learning_rate=1. * 1e-2,
-                 lambdas = [1., 1., 1.],
-                 dims=[784, 250, 100, 10],
-                 act=F.relu,
-                 noise=False,
-                 rc=False,
-                 lateral=False):
-        
-        super(Experiment070_3, self).__init__(
-            device=device,
-            learning_rate=learning_rate,
-            lambdas=lambdas,
-            dims=dims,
-            act=act,
-            noise=noise,
-            rc=rc,
-            lateral=lateral)
-
-        self.kl_loss = self.model.kl_loss
-
-    def forward_for_losses(self, x_l, y_l, x_u, test=False):
-        """
-        Returns
-        -----------
-        tuple:
-            tuple of Variables for separate loss
-        """
-
-        # Forward
-        supervised_losses = []
-        kl_l_losses = []
-        kl_u_losses = []
-        recon_l_losses = []
-        recon_u_losses = []
-
-        x_l_recon_t0 = x_l
-        x_u_recon_t0 = x_u
-        
-        for t in range(self.T):
-            # Supervision for (x_l, y_l)
-            y = self.mlp_enc(x_l_recon_t0, test)
-            supervised_loss = self.supervised_loss(y, y_l)
-            supervised_losses.append(supervised_loss)
-            
-            if t > 1:  # KL Divergence Loss
-                kl_l_loss = self.kl_loss(y, y_p_l)
-                kl_l_losses.append(kl_l_loss)
-            y_p_l = y
-            
-            # Reconstruction for (x_l, )
-            x_l_recon = self.mlp_dec(y, test)
-            recon_loss_l = self.recon_loss(x_l_recon, x_l_recon_t0,  # Virtual AE
-                                               self.mlp_enc.hiddens,
-                                               self.mlp_dec.hiddens)
-            recon_l_losses.append(recon_loss_l)
-            x_l_recon_t0 = x_l_recon
-
-            # Reconstruction for (x_u, _)
-            if x_u is None:
-                recon_u_losses.append(0)
-                kl_l_losses.append(0)
-                kl_u_losses.append(0)
-                continue
-
-            y = self.mlp_enc(x_u_recon_t0, test)
-            x_u_recon = self.mlp_dec(y, test)
-            recon_loss_u = self.recon_loss(x_u_recon,  x_u_recon_t0,  # Virtual AE
-                                               self.mlp_enc.hiddens, 
-                                               self.mlp_dec.hiddens)        
-            recon_u_losses.append(recon_loss_u)
-            x_u_recon_t0 = x_u_recon
-
-            if t > 1:  # KL Divergence Loss
-                kl_u_loss = self.kl_loss(y, y_p_u)
-                kl_u_losses.append(kl_l_loss)
-            y_p_u = y
-
-        # Loss
-        supervised_loss = reduce(lambda x, y: x + y, supervised_losses)
-
-        recon_loss_l = 0
-        recon_loss_u = 0
-        for lambda_, l0, l1 in zip(self.lambdas,  # Use coefficients for ulosses
-                                       recon_l_losses,
-                                       recon_u_losses):
-            recon_loss_l += lambda_ * l0
-            recon_loss_u += lambda_ * l1
-
-        return supervised_loss, recon_loss_l, recon_loss_u, kl_loss
-
 class Experiment000(Experiment):
     """Experiment takes responsibility for a batch not for train-loop.
     """
@@ -432,11 +336,226 @@ class Experiment002(Experiment):
 
         return supervised_loss, recon_loss_l, recon_loss_u, pseudo_supervised_loss
 
+class Experiment017(Experiment):
+    """Experiment takes responsibility for a batch not for train-loop.
+    """
+
+    def __init__(self,
+                 device=None,
+                 learning_rate=1. * 1e-2,
+                 lambdas = [1., 1., 1.],
+                 dims=[784, 250, 100, 10],
+                 act=F.relu,
+                 noise=False,
+                 rc=False,
+                 lateral=False):
+        
+        super(Experiment017, self).__init__(
+            device=device,
+            learning_rate=learning_rate,
+            lambdas=lambdas,
+            dims=dims,
+            act=act,
+            noise=noise,
+            rc=rc,
+            lateral=lateral)
+
+        self.kl_loss = self.model.kl_loss
+
+    def forward_for_losses(self, x_l, y_l, x_u, test=False):
+        """
+        Returns
+        -----------
+        tuple:
+            tuple of Variables for separate loss
+        """
+
+        # Forward
+        supervised_losses = []
+        kl_l_losses = []
+        kl_u_losses = []
+        recon_l_losses = []
+        recon_u_losses = []
+
+        x_l_recon_t0 = x_l
+        x_u_recon_t0 = x_u
+        
+        for t in range(self.T):
+            # Supervision for (x_l, y_l)
+            y = self.mlp_enc(x_l_recon_t0, test)
+            supervised_loss = self.supervised_loss(y, y_l)
+            supervised_losses.append(supervised_loss)
+            
+            if t > 1:  # KL Divergence Loss
+                kl_l_loss = self.kl_loss(y, y_p_l)
+                kl_l_losses.append(kl_l_loss)
+            y_p_l = y
+            
+            # Reconstruction for (x_l, )
+            x_l_recon = self.mlp_dec(y, test)
+            recon_loss_l = self.recon_loss(x_l_recon, x_l_recon_t0,  # Virtual AE
+                                               self.mlp_enc.hiddens,
+                                               self.mlp_dec.hiddens)
+            recon_l_losses.append(recon_loss_l)
+            x_l_recon_t0 = x_l_recon
+
+            # Reconstruction for (x_u, _)
+            if x_u is None:
+                recon_u_losses.append(0)
+                kl_l_losses.append(0)
+                kl_u_losses.append(0)
+                continue
+
+            y = self.mlp_enc(x_u_recon_t0, test)
+            x_u_recon = self.mlp_dec(y, test)
+            recon_loss_u = self.recon_loss(x_u_recon,  x_u_recon_t0,  # Virtual AE
+                                               self.mlp_enc.hiddens, 
+                                               self.mlp_dec.hiddens)        
+            recon_u_losses.append(recon_loss_u)
+            x_u_recon_t0 = x_u_recon
+
+            if t > 1:  # KL Divergence Loss
+                kl_u_loss = self.kl_loss(y, y_p_u)
+                kl_u_losses.append(kl_l_loss)
+            y_p_u = y
+
+        # Loss
+        supervised_loss = reduce(lambda x, y: x + y, supervised_losses)
+
+        recon_loss_l = 0
+        recon_loss_u = 0
+        for lambda_, l0, l1 in zip(self.lambdas,  # Use coefficients for ulosses
+                                       recon_l_losses,
+                                       recon_u_losses):
+            recon_loss_l += lambda_ * l0
+            recon_loss_u += lambda_ * l1
+
+        return supervised_loss, recon_loss_l, recon_loss_u, kl_loss
+
+class Experiment019(Experiment):
+    """Experiment takes responsibility for a batch not for train-loop.
+    """
+
+    def __init__(self,
+                 device=None,
+                 learning_rate=1. * 1e-2,
+                 lambdas = [1., 1., 1.],
+                 dims=[784, 250, 100, 10],
+                 act=F.relu,
+                 noise=False,
+                 rc=False,
+                 lateral=False):
+        
+        super(Experiment019, self).__init__(
+            device=device,
+            learning_rate=learning_rate,
+            lambdas=lambdas,
+            dims=dims,
+            act=act,
+            noise=noise,
+            rc=rc,
+            lateral=lateral)
+
+        self.kl_loss = self.model.kl_loss
+
+    def forward_for_losses(self, x_l, y_l, x_u, test=False):
+        """
+        Returns
+        -----------
+        tuple:
+            tuple of Variables for separate loss
+        """
+
+        # Forward
+        supervised_losses = []
+        kl_l_losses = []
+        kl_u_losses = []
+        recon_l_losses = []
+        recon_u_losses = []
+
+        x_l_recon_t0 = x_l
+        x_u_recon_t0 = x_u
+        
+        for t in range(self.T):
+            # Supervision for (x_l, y_l)
+            y = self.mlp_enc(x_l_recon_t0, test)
+            supervised_loss = self.supervised_loss(y, y_l)
+            supervised_losses.append(supervised_loss)
+            
+            if t > 1:  # KL Divergence Loss
+                kl_l_loss = self.kl_loss(y, y_p_l)
+                kl_l_losses.append(kl_l_loss)
+            y_p_l = y
+            
+            # Reconstruction for (x_l, )
+            recon_loss_l = 0
+            if t > 1:  # RC for enc(t+1) and dec(t)
+                recon_loss_l = self.recon_loss(
+                    None, None, self.mlp_enc.hiddens, mlp_dec_l_hiddens)
+            x_l_recon = self.mlp_dec(y, test)
+            mlp_dec_l_hiddens = self.mlp_dec.hiddens
+            recon_loss_l += self.recon_loss(
+                x_l_recon, x_l_recon_t0,  None, None)  # Virtual AE
+            x_l_recon_t0 = x_l_recon
+            if t == self.T - 1:
+                self.mlp_enc(x_l_recon_t0, test)
+                recon_loss_l += self.recon_loss(
+                    None, None, self.mlp_enc.hiddens, self.mlp_dec.hiddens)
+            recon_l_losses.append(recon_loss_l)
+
+            # Reconstruction for (x_u, _)
+            if x_u is None:
+                recon_u_losses.append(0)
+                kl_l_losses.append(0)
+                kl_u_losses.append(0)
+                continue
+
+            y = self.mlp_enc(x_u_recon_t0, test)
+            recon_loss_u = 0
+            if t > 1:  # RC for enc(t+1) and dec(t)
+                recon_loss_u = self.recon_loss(
+                    None, None, self.mlp_enc.hiddens, mlp_dec_u_hiddens)
+            x_u_recon = self.mlp_dec(y, test)
+            mlp_dec_u_hiddens = self.mlp_dec.hiddens
+            recon_loss_u += self.recon_loss(
+                x_u_recon, x_u_recon_t0,  None, None)  # Virtual AE
+            x_u_recon_t0 = x_u_recon
+            
+            if t == self.T - 1:
+                self.mlp_enc(x_u_recon_t0, test)
+                recon_loss_u += self.recon_loss(
+                    None, None, self.mlp_enc.hiddens, self.mlp_dec.hiddens)
+            recon_u_losses.append(recon_loss_u)
+
+            if t > 1:  # KL Divergence Loss
+                kl_u_loss = self.kl_loss(y, y_p_u)
+                kl_u_losses.append(kl_l_loss)
+            y_p_u = y
+
+        # Loss
+        supervised_loss = reduce(lambda x, y: x + y, supervised_losses)
+
+        recon_loss_l = 0
+        recon_loss_u = 0
+        for lambda_, l0, l1 in zip(self.lambdas,  # Use coefficients for ulosses
+                                       recon_l_losses,
+                                       recon_u_losses):
+            recon_loss_l += lambda_ * l0
+            recon_loss_u += lambda_ * l1
+
+        return supervised_loss, recon_loss_l, recon_loss_u, kl_loss
+
 # Alias
 Experiment001 = Experiment000
 Experiment005 = Experiment000
-
 Experiment004 = Experiment002
 Experiment006 = Experiment002
 Experiment008 = Experiment002
+
+Experiment018 = Experiment017
+Experiment022 = Experiment017
+Experiment021 = Experiment019
+Experiment023 = Experiment019
+Experiment025 = Experiment019
+
 
