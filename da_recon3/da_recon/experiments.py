401638df8ec9b34fc96db0a8e8b40144def6d88a
kzky <rkzfilter@gmail.com>
2016-12-03 19:03:47 +0900

    Move important scripts only

---
+++ da_recon3/da_recon/experiments.py
@@ -0,0 +1,391 @@
+from da_recon.models import MLPEncDecModel
+from chainer import optimizers, Variable
+import chainer.functions as F
+
+class Experiment(object):
+    """Experiment takes responsibility for a batch not for train-loop.
+    """
+
+    def __init__(self,
+                 device=None,
+                 learning_rate=1. * 1e-2,
+                 lambdas = [1., 1., 1.],
+                 dims=[784, 250, 100, 10],
+                 act=F.relu,
+                 noise=False,
+                 lateral=False):
+
+        # Settting
+        self.device = device
+        self.lambdas = lambdas
+        self.T = len(lambdas)
+        
+        # Model
+        self.model = MLPEncDecModel(
+            dims=dims, act=act,
+            noise=noise, lateral=lateral,
+            device=device)
+        self.model.to_gpu(self.device) if self.device else None
+        self.mlp_enc = self.model.mlp_enc
+        self.mlp_dec = self.model.mlp_dec
+        self.supervised_loss = self.model.supervised_loss
+        self.recon_loss = self.model.recon_loss
+
+        # Optimizer
+        self.optimizer = optimizers.Adam(learning_rate)
+        self.optimizer.setup(self.model)
+        self.optimizer.use_cleargrads()
+        
+    def train(self, x_l, y_l, x_u):
+        loss = self.forward(x_l, y_l, x_u)
+        self.backward(loss)
+        self.update()
+
+    def forward_for_losses(self, x_l, y_l, x_u, test=False):
+        """
+        Returns
+        -----------
+        tuple:
+            tuple of Variables for separate loss
+        """
+
+        # Forward
+        supervised_losses = []
+        recon_l_losses = []
+        recon_u_losses = []
+
+        x_l_recon = x_l
+        x_u_recon = x_u
+        for t in range(self.T):
+            # Supervision for (x_l, y_l)
+            y = self.mlp_enc(x_l_recon, test)
+            
+            supervised_loss = self.supervised_loss(y, y_l)
+            supervised_losses.append(supervised_loss)
+            
+            # Reconstruction for (x_l, )
+            x_l_recon = self.mlp_dec(y, test)
+            recon_loss_l = self.recon_loss(x_l_recon, x_l,  # Use self, x_l
+                                               self.mlp_enc.hiddens,
+                                               self.mlp_dec.hiddens)
+            recon_l_losses.append(recon_loss_l)
+                
+            # Reconstruction for (x_u, _)
+            if x_u is None:
+                recon_u_losses.append(0)
+                continue
+
+            y = self.mlp_enc(x_u_recon, test)
+            x_u_recon = self.mlp_dec(y, test)
+            recon_loss_u = self.recon_loss(x_u_recon, x_u,  # Use self, x_u
+                                               self.mlp_enc.hiddens, 
+                                               self.mlp_dec.hiddens)
+            recon_u_losses.append(recon_loss_u)
+
+        # Loss
+        supervised_loss = reduce(lambda x, y: x + y, supervised_losses)
+
+        recon_loss_l = 0
+        recon_loss_u = 0
+        for lambda_, l0, l1 in zip(self.lambdas,  # Use coefficients for ulosses
+                                       recon_l_losses,
+                                       recon_u_losses):
+            recon_loss_l += lambda_ * l0
+            recon_loss_u += lambda_ * l1
+
+        return supervised_loss, recon_loss_l, recon_loss_u
+
+    def forward(self, x_l, y_l, x_u, test):
+        losses = self.forward_for_losses(x_l, y_l, x_u)
+        return reduce(lambda x, y: x + y, losses)
+
+    def backward(self, loss):
+        self.model.cleargrads()
+        loss.backward()
+
+    def update(self, ):
+        self.optimizer.update()
+
+    def test(self, x_l, y_l):
+        y = self.mlp_enc(x_l, test=True)
+        acc = F.accuracy(y, y_l)
+        losses = self.forward_for_losses(x_l, y_l, None, test=True)
+        supervised_loss = losses[0]
+        recon_loss = losses[1]
+        return acc, supervised_loss, recon_loss
+        
+class Experiment006(Experiment):
+    """Experiment takes responsibility for a batch not for train-loop.
+    """
+
+    def __init__(self,
+                 device=None,
+                 learning_rate=1. * 1e-2,
+                 lambdas = [1., 1., 1.],
+                 dims=[784, 250, 100, 10],
+                 act=F.relu,
+                 noise=False,
+                 lateral=False,
+                 test=False):
+
+        super(Experiment006, self).__init__(
+            device=device,
+            learning_rate=learning_rate,
+            lambdas=lambdas,
+            dims=dims,
+            act=act,
+            noise=noise,
+            lateral=lateral,
+            test=test)
+
+    def forward_for_losses(self, x_l, y_l, x_u, test=False):
+        """
+        Returns
+        -----------
+        tuple:
+            tuple of Variables for separate loss
+        """
+
+        # Forward
+        supervised_losses = []
+        recon_l_losses = []
+        recon_u_losses = []
+
+        x_l_recon_t0 = x_l
+        x_u_recon_t0 = x_u
+        for t in range(self.T):
+            # Supervision for (x_l, y_l)
+            y = self.mlp_enc(x_l_recon_t0, test)
+            supervised_loss = self.supervised_loss(y, y_l)
+            supervised_losses.append(supervised_loss)
+            
+            # Reconstruction for (x_l, )
+            x_l_recon = self.mlp_dec(y, test)
+            recon_loss_l = self.recon_loss(x_l_recon, x_l_recon_t0,  # Virtual AE
+                                               self.mlp_enc.hiddens,
+                                               self.mlp_dec.hiddens)
+            recon_l_losses.append(recon_loss_l)
+            x_l_recon_t0 = x_l_recon
+
+            # Reconstruction for (x_u, _)
+            if x_u is None:
+                recon_u_losses.append(0)
+                continue
+
+            y = self.mlp_enc(x_u_recon_t0, test)
+            x_u_recon = self.mlp_dec(y, test)
+            recon_loss_u = self.recon_loss(x_u_recon,  x_u_recon_t0,  # Virtual AE
+                                               self.mlp_enc.hiddens, 
+                                               self.mlp_dec.hiddens)        
+            recon_u_losses.append(recon_loss_u)
+            x_u_recon_t0 = x_u_recon
+
+        # Loss
+        supervised_loss = reduce(lambda x, y: x + y, supervised_losses)
+
+        recon_loss_l = 0
+        recon_loss_u = 0
+        for lambda_, l0, l1 in zip(self.lambdas,  # Use coefficients for ulosses
+                                       recon_l_losses,
+                                       recon_u_losses):
+            recon_loss_l += lambda_ * l0
+            recon_loss_u += lambda_ * l1
+
+        return supervised_loss, recon_loss_l, recon_loss_u
+
+class Experiment070_2(Experiment006):
+    """Experiment takes responsibility for a batch not for train-loop.
+    """
+
+    def __init__(self,
+                 device=None,
+                 learning_rate=1. * 1e-2,
+                 lambdas = [1., 1., 1.],
+                 dims=[784, 250, 100, 10],
+                 act=F.relu,
+                 noise=False,
+                 lateral=False,
+                 test=False):
+
+        super(Experiment070_2, self).__init__(
+            device=device,
+            learning_rate=learning_rate,
+            lambdas=lambdas,
+            dims=dims,
+            act=act,
+            noise=noise,
+            lateral=lateral,
+            test=test)
+
+        self.pseudo_supervised_loss = self.model.pseudo_supervised_loss
+
+    def forward_for_losses(self, x_l, y_l, x_u, test=False):
+        """
+        Returns
+        -----------
+        tuple:
+            tuple of Variables for separate loss
+        """
+
+        # Forward
+        supervised_losses = []
+        pseudo_supervised_l_losses = []
+        pseudo_supervised_u_losses = []
+        recon_l_losses = []
+        recon_u_losses = []
+
+        x_l_recon_t0 = x_l
+        x_u_recon_t0 = x_u
+        
+        for t in range(self.T):
+            # Supervision for (x_l, y_l)
+            y = self.mlp_enc(x_l_recon_t0, test)
+            supervised_loss = self.supervised_loss(y, y_l)
+            supervised_losses.append(supervised_loss)
+            
+            if t > 1:  # Pseudo Supervised Loss
+                pseudo_supervised_l_loss = self.pseudo_supervised_loss(y, y_p_l)
+                pseudo_supervised_l_losses.append(pseudo_supervised_l_loss)
+            y_p_l = y
+            
+            # Reconstruction for (x_l, )
+            x_l_recon = self.mlp_dec(y, test)
+            recon_loss_l = self.recon_loss(x_l_recon, x_l_recon_t0,  # Virtual AE
+                                               self.mlp_enc.hiddens,
+                                               self.mlp_dec.hiddens)
+            recon_l_losses.append(recon_loss_l)
+            x_l_recon_t0 = x_l_recon
+
+            # Reconstruction for (x_u, _)
+            if x_u is None:
+                recon_u_losses.append(0)
+                pseudo_supervised_l_losses.append(0)
+                pseudo_supervised_u_losses.append(0)
+                continue
+
+            y = self.mlp_enc(x_u_recon_t0, test)
+            x_u_recon = self.mlp_dec(y, test)
+            recon_loss_u = self.recon_loss(x_u_recon,  x_u_recon_t0,  # Virtual AE
+                                               self.mlp_enc.hiddens, 
+                                               self.mlp_dec.hiddens)        
+            recon_u_losses.append(recon_loss_u)
+            x_u_recon_t0 = x_u_recon
+
+            if t > 1:  # Pseudo Supervised Loss
+                pseudo_supervised_u_loss = self.pseudo_supervised_loss(y, y_p_u)
+                pseudo_supervised_u_losses.append(pseudo_supervised_l_loss)
+            y_p_u = y
+
+        # Loss
+        supervised_loss = reduce(lambda x, y: x + y, supervised_losses)
+
+        recon_loss_l = 0
+        recon_loss_u = 0
+        for lambda_, l0, l1 in zip(self.lambdas,  # Use coefficients for ulosses
+                                       recon_l_losses,
+                                       recon_u_losses):
+            recon_loss_l += lambda_ * l0
+            recon_loss_u += lambda_ * l1
+
+        # Pseudo Supervised Loss
+        pseudo_supervised_loss = 0
+        for lambda_, l0, l1 in zip(self.lambdas, pseudo_supervised_l_losses,
+                                       pseudo_supervised_u_losses):
+            pseudo_supervised_loss += lambda_ * (l0 + l1)
+
+        return supervised_loss, recon_loss_l, recon_loss_u, pseudo_supervised_loss
+
+class Experiment070_3(Experiment006):
+    """Experiment takes responsibility for a batch not for train-loop.
+    """
+
+    def __init__(self,
+                 device=None,
+                 learning_rate=1. * 1e-2,
+                 lambdas = [1., 1., 1.],
+                 dims=[784, 250, 100, 10],
+                 act=F.relu,
+                 noise=False,
+                 lateral=False):
+        
+        super(Experiment070_3, self).__init__(
+            device=device,
+            learning_rate=learning_rate,
+            lambdas=lambdas,
+            dims=dims,
+            act=act,
+            noise=noise,
+            lateral=lateral)
+
+        self.kl_loss = self.model.kl_loss
+
+    def forward_for_losses(self, x_l, y_l, x_u, test=False):
+        """
+        Returns
+        -----------
+        tuple:
+            tuple of Variables for separate loss
+        """
+
+        # Forward
+        supervised_losses = []
+        kl_l_losses = []
+        kl_u_losses = []
+        recon_l_losses = []
+        recon_u_losses = []
+
+        x_l_recon_t0 = x_l
+        x_u_recon_t0 = x_u
+        
+        for t in range(self.T):
+            # Supervision for (x_l, y_l)
+            y = self.mlp_enc(x_l_recon_t0, test)
+            supervised_loss = self.supervised_loss(y, y_l)
+            supervised_losses.append(supervised_loss)
+            
+            if t > 1:  # KL Divergence Loss
+                kl_l_loss = self.kl_loss(y, y_p_l)
+                kl_l_losses.append(kl_l_loss)
+            y_p_l = y
+            
+            # Reconstruction for (x_l, )
+            x_l_recon = self.mlp_dec(y, test)
+            recon_loss_l = self.recon_loss(x_l_recon, x_l_recon_t0,  # Virtual AE
+                                               self.mlp_enc.hiddens,
+                                               self.mlp_dec.hiddens)
+            recon_l_losses.append(recon_loss_l)
+            x_l_recon_t0 = x_l_recon
+
+            # Reconstruction for (x_u, _)
+            if x_u is None:
+                recon_u_losses.append(0)
+                kl_l_losses.append(0)
+                kl_u_losses.append(0)
+                continue
+
+            y = self.mlp_enc(x_u_recon_t0, test)
+            x_u_recon = self.mlp_dec(y, test)
+            recon_loss_u = self.recon_loss(x_u_recon,  x_u_recon_t0,  # Virtual AE
+                                               self.mlp_enc.hiddens, 
+                                               self.mlp_dec.hiddens)        
+            recon_u_losses.append(recon_loss_u)
+            x_u_recon_t0 = x_u_recon
+
+            if t > 1:  # KL Divergence Loss
+                kl_u_loss = self.kl_loss(y, y_p_u)
+                kl_u_losses.append(kl_l_loss)
+            y_p_u = y
+
+        # Loss
+        supervised_loss = reduce(lambda x, y: x + y, supervised_losses)
+
+        recon_loss_l = 0
+        recon_loss_u = 0
+        for lambda_, l0, l1 in zip(self.lambdas,  # Use coefficients for ulosses
+                                       recon_l_losses,
+                                       recon_u_losses):
+            recon_loss_l += lambda_ * l0
+            recon_loss_u += lambda_ * l1
+
+        return supervised_loss, recon_loss_l, recon_loss_u, kl_loss
+
---
+++ da_recon3/da_recon/models.py
@@ -0,0 +1,285 @@
+"""Models
+"""
+import numpy as np
+import chainer
+import chainer.variable as variable
+from chainer.functions.activation import lstm
+from chainer import cuda, Function, gradient_check, report, training, utils, Variable
+from chainer import datasets, iterators, optimizers, serializers
+from chainer import Link, Chain, ChainList
+import chainer.functions as F
+import chainer.links as L
+from collections import OrderedDict
+import logging
+import time
+from utils import to_device
+
+class MLPEnc(Chain):
+
+    def __init__(self,
+                     dims,
+                     act=F.relu,
+                     noise=False,
+                     lateral=False,
+                     device=None):
+
+        # Setup layers
+        layers = {}
+        linears = OrderedDict()
+        batch_norms = OrderedDict()
+        for l, d in enumerate(zip(dims[0:-1], dims[1:])):
+            d_in, d_out = d[0], d[1]
+
+            # Linear
+            linear = L.Linear(d_in, d_out)
+            l_name = "linear-enc-{:03}".format(l)
+            linears[l_name] = linear
+
+            # Normalization and BatchCorrection
+            batch_norm = L.BatchNormalization(d_out, decay=0.9)
+            bn_name = "bn-enc-{:03d}".format(l)
+            batch_norms[bn_name] = batch_norm
+
+        layers.update(linears)
+        layers.update(batch_norms)
+        
+        super(MLPEnc, self).__init__(**layers)
+        self.dims = dims
+        self.layers = layers
+        self.linears = linears
+        self.batch_norms = batch_norms
+        self.act = act
+        self.noise = noise
+        self.lateral = lateral
+        self.device = device
+        self.hiddens = []
+
+    def __call__(self, x, test):
+        h = x
+        self.hiddens = []
+        for i, layers in enumerate(zip(
+            self.linears.values(), self.batch_norms.values())):
+
+            linear, batch_norm = layers
+
+            # Add noise
+            if self.noise and not self.bn and not self.lateral and not test:
+                if np.random.randint(0, 2):
+                    n = np.random.normal(0, 0.03, h.data.shape).astype(np.float32)
+                    n_ = Variable(to_device(n, self.device))
+                    h = h + n_
+
+            # Linear
+            h = linear(h)
+
+            # Batchnorm
+            h = batch_norm(h, test)
+            if self.noise and not test:
+                n = np.random.normal(0, 0.03, h.data.shape).astype(np.float32)
+                n_ = Variable(to_device(n, self.device))
+                h = h + n_
+            
+            if self.lateral and i != len(self.dims) - 2:
+                self.hiddens.append(h)
+
+            # Activation
+            h = self.act(h)
+
+        return h
+
+class Denoise(Chain):
+    def __init__(self, dim):
+        super(Denoise, self).__init__(
+            a0=L.Scale(W_shape=(dim, )),
+            a1=L.Scale(W_shape=(dim, )),
+            a2=L.Scale(W_shape=(dim, )),
+            a3=L.Bias(shape=(dim, )),
+            a4=L.Bias(shape=(dim, )),
+            b0=L.Scale(W_shape=(dim, )),
+            b1=L.Scale(W_shape=(dim, )),
+            b2=L.Scale(W_shape=(dim, )),
+            b3=L.Bias(shape=(dim, )),
+            )
+
+    def __call__(self, x, y):
+        xy = x * y
+        a = self.a3(self.a0(x) + self.a1(y) + self.a2(xy))
+        b = self.b3(self.b0(x) + self.b1(y) + self.b2(xy))
+        
+        return b + self.a4(F.sigmoid(a))
+
+class MLPDec(Chain):
+
+    def __init__(self, dims, act=F.relu,
+                     lateral=False,
+                     mlp_enc=None,
+                     device=None):
+        # Setup layers
+        layers = {}
+        linears = OrderedDict()
+        batch_norms = OrderedDict()
+        denoises = OrderedDict()
+        
+        dims_reverse = dims[::-1]
+        for l, d in enumerate(zip(dims_reverse[0:-1], dims_reverse[1:])):
+            d_in, d_out = d[0], d[1]
+
+            # Linear
+            linear = L.Linear(d_in, d_out)
+            l_name = "linear-dec-{:03}".format(l)
+            linears[l_name] = linear
+
+            # Normalization and BatchCorrection
+            batch_norm = L.BatchNormalization(d_out, decay=0.9)
+            bn_name = "bn-dec-{:03d}".format(l)
+            batch_norms[bn_name] = batch_norm
+
+            # Denoise
+            if lateral and l != 0:
+                dn_name = "dn-dec-{:03d}".format(l)
+                denoises[dn_name] = Denoise(d_in)
+                                
+        layers.update(linears)
+        layers.update(batch_norms) if bn else None
+        layers.update(denoises) if lateral else None
+        
+        super(MLPDec, self).__init__(**layers)
+        self.dims = dims
+        self.layers = layers
+        self.linears = linears
+        self.batch_norms = batch_norms
+        self.denoises = denoises
+        self.act = act
+        self.lateral = lateral
+        self.device = device
+        self.hiddens = []
+        self.mlp_enc = mlp_enc
+            
+    def __call__(self, x, test):
+        h = x
+        self.hiddens = []
+        for i, layers in enumerate(zip(
+            self.linears.values(), self.batch_norms.values())):
+            linear, batch_norm = layers
+
+            # Linear
+            h = linear(h)
+
+            # Batchnorm
+            if self.bn or self.lateral:
+                h = batch_norm(h, test)
+
+            # Activation
+            if not self.lateral:
+                h = self.act(h)
+
+            # Denoise
+            if self.lateral and i != len(self.dims) - 2:
+                self.hiddens.append(h)
+                denoise = self.denoises.values()[i]
+                h_enc = self.mlp_enc.hiddens[::-1][i]
+                h = denoise(h_enc, h)
+
+        return h
+            
+class SupervizedLoss(Chain):
+
+    def __init__(self, ):
+        super(SupervizedLoss, self).__init__()
+        self.loss = None
+        
+    def __call__(self, y, t):
+        self.loss = F.softmax_cross_entropy(y, t)
+        return self.loss
+
+class ReconstructionLoss(Chain):
+
+    def __init__(self,
+                     noise=False,
+                     lateral=False):
+
+        super(ReconstructionLoss, self).__init__()
+        self.noise = noise
+        self.lateral = lateral
+        self.loss = None
+        
+    def __call__(self, x_recon, x, enc_hiddens, dec_hiddens):
+        """
+        Parameters
+        -----------------
+        x_recon: Variable to be reconstructed as label
+        x: Variable to be reconstructed as label
+        enc_hiddens: list of Variable
+        dec_hiddens: list of Varialbe
+        """
+
+        # Lateral Recon Loss
+        if self.lateral: #TODO: do something
+            pass
+
+        # Reconstruction Loss
+        d = np.prod(x.data.shape[1:])
+        recon_loss = F.mean_squared_error(x_recon, x) / d
+
+        self.loss = recon_loss  #TODO: Loss add lateral recon loss
+        
+        return self.loss
+
+class PseudoSupervisedLoss(Chain):
+    """Compute cross entropy between y_t and y_{t+1}.
+    """
+
+    def __init__(self, ):
+
+    def __call__(self, y, t):
+        t_normalized = F.softmax(t)
+        y_log_softmax = F.log_softmax(y)
+        n = y.data.shape[0]
+
+        return - F.sum(t_normalized * y_log_softmax) / n
+
+class KLLoss(Chain):
+    """Compute cross entropy between y_t and y_{t+1}.
+    """
+
+    def __init__(self, ):
+
+    def __call__(self, y, t):
+        t_normalized = F.softmax(t)
+        t_log_softmax = F.log_softmax(t)
+        y_log_softmax = F.log_softmax(y)
+        n = y.data.shape[0]
+
+        return F.sum((t_normalized * t_log_softmax) \
+                         - (t_normalized * y_log_softmax)) / n
+
+class MLPEncDecModel(Chain):
+    def __init__(self,
+                     dims,
+                     act=F.relu,
+                     noise=False,
+                     lateral=False,
+                     device=None):
+        # Constrcut models
+        mlp_enc = MLPEnc(
+            dims=dims,
+            act=act,
+            noise=noise,
+            lateral=lateral,
+            device=device)
+        mlp_dec = MLPDec(
+            dims=dims,
+            act=act,
+            lateral=lateral,
+            mlp_enc=mlp_enc,
+            device=device)
+        self.supervised_loss = SupervizedLoss()
+        self.recon_loss = ReconstructionLoss()
+        self.pseudo_supervised_loss = PseudoSupervisedLoss()
+
+        super(MLPEncDecModel, self).__init__(
+            mlp_enc=mlp_enc,
+            mlp_dec=mlp_dec)
+
+    def __call__(self, x_l, y_l, x_u, y_u):
+        pass
