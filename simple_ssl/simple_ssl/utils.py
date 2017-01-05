1b4600dc5dd526acc1398aabcbf920a85c733f5a
kzky <rkzfilter@gmail.com>
2017-01-05 22:30:39 +0900

    Add noise function

--- simple_ssl/simple_ssl/utils.py
+++ simple_ssl/simple_ssl/utils.py
@@ -1,9 +1,21 @@
 import numpy as np
 import os
-from chainer import cuda
+from chainer import cuda, Variable
 
 def to_device(x, device=None):
     if device:
         return cuda.to_gpu(x, device)
     else:
         return cuda.to_cpu(x)
+
+
+def add_normal_noise(h, sigma=0.03):
+    if np.random.randint(0, 2):
+        n = np.random.normal(0, sigma, h.data.shape).astype(np.float32)
+        device = cuda.get_device(h)
+        if device.id ==  -1:
+            n_ = Variable(n)
+        else:
+            n_ = Variable(cuda.(n, device.id))
+        h = h + n_
+    return h
