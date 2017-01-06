c061bb40966a06676db8d167f25e981fad24ea24
kzky <rkzfilter@gmail.com>
2017-01-06 21:48:57 +0900

    Add grad norm hook.

--- simple_ssl/simple_ssl/experiments.py
+++ simple_ssl/simple_ssl/experiments.py
@@ -1,6 +1,7 @@
 from simple_ssl.models import MLPEncDecModel, NegativeEntropyLoss
 from chainer import optimizers, Variable
 import chainer.functions as F
+from utils import grad_norm_hook
 
 class Experiment(object):
     """Experiment takes responsibility for a batch not for train-loop.
@@ -42,10 +43,12 @@ class Experiment(object):
         self.optimizer = optimizers.Adam(learning_rate)
         self.optimizer.setup(self.model)
         self.optimizer.use_cleargrads()
+        self.optimizer.add_hook(grad_norm_hook, "grad_norm_hook")
         
     def train(self, x_l, y_l, x_u):
         loss = self.forward(x_l, y_l, x_u)
         self.backward(loss)
+        #self.optimizer.call_hooks()
         self.update()
 
     def forward_for_losses(self, x_l, y_l, x_u, test=False):
--- simple_ssl/simple_ssl/utils.py
+++ simple_ssl/simple_ssl/utils.py
@@ -19,3 +19,16 @@ def add_normal_noise(h, sigma=0.03):
             n_ = Variable(cuda.to_gpu(n, device.id))
         h = h + n_
     return h
+
+def grad_norm_hook(optimizer):
+    for p in optimizer.target.params():
+        grad_data = p.grad
+        shape = grad_data.shape
+        reshape = (1, np.prod(shape), )
+
+        grad = Variable(grad_data)
+        grad_reshape = F.reshape(grad, reshape)
+        grad_norm = F.normalize(grad_reshape)
+        grad_norm_reshape = F.reshape(grad_norm, shape)
+
+        p.grad = grad_norm_reshape.data
