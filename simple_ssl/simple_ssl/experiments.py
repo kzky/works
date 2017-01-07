aa49a75aa4d420a9ac0e473bb3a0d3ab756fcc78
kzky <rkzfilter@gmail.com>
2017-01-07 19:31:35 +0900

    Add exp1000

--- simple_ssl/simple_ssl/experiments.py
+++ simple_ssl/simple_ssl/experiments.py
@@ -133,6 +133,86 @@ class Experiment(object):
         loss = self.forward_for_losses(x_l, y_l, None, test=True)  # only measure x_l
         supervised_loss = loss
         return acc, supervised_loss
+
+
+class Experiment1000(Experiment):
+    """Experiment takes responsibility for a batch not for train-loop.
+    """
+
+    def __init__(self,
+                 device=None,
+                 learning_rate=1. * 1e-2,
+                 dims=[784, 250, 100, 10],
+                 act=F.relu,
+                 noise=False,
+                 rc=False,
+                 lds=False,
+                 scale_rc=False,
+                 scale_lds=False,
+                 ):
+
+        # Settting
+        self.device = device
+        self.rc = rc
+        self.lds = lds
+        self.scale_rc = scale_rc
+        self.scale_lds = scale_lds
         
+        # Model
+        self.model = MLPEncDecModel(
+            dims=dims, act=act,
+            noise=noise, rc=rc,
+            device=device)
+        self.model.to_gpu(self.device) if self.device else None
+        self.mlp_enc = self.model.mlp_enc
+        self.mlp_dec = self.model.mlp_dec
+        self.supervised_loss = self.model.supervised_loss
+        self.recon_loss = self.model.recon_loss
+        self.neg_ent_loss = self.model.neg_ent_loss
+        self.graph_loss = self.model.graph_loss
+        self.kl_recon_loss = self.model.kl_recon_loss
+        self.cc_loss = self.model.cc_loss
+
+        # Optimizer
+        self.optimizer_ce = optimizers.Adam(learning_rate)
+        self.optimizer_ce.setup(self.model)
+        self.optimizer_ce.use_cleargrads()
+
+        self.optimizer_rc = optimizers.Adam(learning_rate)
+        self.optimizer_rc.setup(self.model)
+        self.optimizer_rc.use_cleargrads()
+
+        self.optimizer_ne = optimizers.Adam(learning_rate)
+        self.optimizer_ne.setup(self.model)
+        self.optimizer_ne.use_cleargrads()
+                
+    def train(self, x_l, y_l, x_u, test=False):
+        # Supervision for (x_l, y_l)
+        y = self.mlp_enc(x_l, test)
+        supervised_loss = self.supervised_loss(y, y_l)
+        self.optimizer_ce.cleargrads()
+        supervised_loss.backward()
+        self.optimizer_ce.update()
+
+        # Reconstruction for (x_u, _)
+        y = self.mlp_enc(x_u, test)
+        x_u_recon = self.mlp_dec(y, test)
+        recon_loss_u = self.recon_loss(x_u_recon, x_u,  # Use self, x_u
+                                       self.mlp_enc.hiddens, 
+                                       self.mlp_dec.hiddens, 
+                                       self.scale_rc)
+        self.optimizer_rc.cleargrads()
+        recon_loss_u.backward()
+        self.optimizer_rc.update()
+
+        # Negative Entropy for y_u
+        if self.lds:
+            neg_ent_u = self.neg_ent_loss(y, self.mlp_enc.hiddens, scale=self.scale_lds) 
+        else:
+            neg_ent_u = self.neg_ent_loss(y, scale=self.scale_lds)
+        self.optimizer_ne.cleargrads()
+        neg_ent_u.backward()
+        self.optimizer_ne.update()
+
 # Alias
 Experiment000 = Experiment
