de881b07737749070ed8d5801ea6a02cc2790175
kzky <rkzfilter@gmail.com>
2017-02-05 00:25:32 +0900

    Add JSD experiment.

--- lds/lds/experiments.py
+++ lds/lds/experiments.py
@@ -17,7 +17,7 @@ import shutil
 import csv
 from utils import to_device
 from chainer_fix import BatchNormalization
-from losses import ReconstructionLoss, NegativeEntropyLoss, GANLoss
+from losses import ReconstructionLoss, NegativeEntropyLoss, GANLoss, JensenShannonDivergenceLoss
 from sklearn.metrics import confusion_matrix
 from lds.cnn_model import AutoEncoder
 
@@ -277,6 +277,95 @@ class Experiment002(Experiment001):
         
         # loss for unlabeled samples
         loss_u = l_ne_u + l_rec_u + l_lrec_u
+
+        loss = loss_l + loss_u
+
+        # Backward and Update
+        self.ae.cleargrads()
+        loss.backward()
+        self.optimizer.update()
+    
+class Experiment003(Experiment002):
+    """Decoder predict label.
+    It uses NE loss on these predictions and Jensen Shannon Divergence 
+    between predictions between encoder and decoder.
+    """
+
+    def __init__(self, device=None, learning_rate=1e-3, act=F.relu):
+        super(Experiment003, self).__init__(
+            device=device, learning_rate=learning_rate, act=act
+        )
+
+        self.jsd_loss = JensenShannonDivergenceLoss()
+        
+    def train(self, x_l, y_l, x_u):
+        # Labeled samples
+        y = self.ae.encoder(x_l)
+        x_rec = self.ae.decoder(y)
+
+        # cronss entropy loss
+        l_ce_l = 0
+        l_ce_l += F.softmax_cross_entropy(y, y_l) \
+                  + reduce(lambda x, y: x + y, 
+                           [F.softmax_cross_entropy(y_, y_l) \
+                            for y_ in self.ae.encoder.classifiers]) \
+                                + reduce(lambda x, y: x + y, 
+                                         [F.softmax_cross_entropy(y_, y_l) \
+                                          for y_ in self.ae.decoder.classifiers])
+
+        # negative entropy loss
+        l_ne_l = 0
+        l_ne_l += self.ne_loss(y) \
+                  + reduce(lambda x, y: x + y, 
+                           [self.ne_loss(y_) for y_ in self.ae.encoder.classifiers]) \
+                           + reduce(lambda x, y: x + y, 
+                                    [self.ne_loss(y_) for y_ in self.ae.decoder.classifiers])
+        
+        # reconstruction loss
+        l_rec_l = 0
+        l_rec_l += self.recon_loss(x_l, x_rec) \
+                   + reduce(lambda x, y: x + y,
+                            [self.recon_loss(x, y) for x, y in zip(
+                                self.ae.encoder.hiddens,
+                                self.ae.decoder.hiddens[::-1])])
+
+        # label jsd loss
+        l_ljsd_l = reduce(lambda x, y: x + y,
+                          [self.jsd_loss(x, y) for x, y in zip(
+                              self.ae.encoder.classifiers,
+                              self.ae.decoder.classifiers[::-1])])
+
+        # loss for labeled samples
+        loss_l = l_ce_l + l_ne_l + l_rec_l + l_ljsd_l
+
+        # Unlabeled samples
+        y = self.ae.encoder(x_u)
+        x_rec = self.ae.decoder(y)
+
+        # negative entropy loss
+        l_ne_u = 0
+        l_ne_u += self.ne_loss(y) \
+                  + reduce(lambda x, y: x + y, 
+                           [self.ne_loss(y_) for y_ in self.ae.encoder.classifiers]) \
+                           + reduce(lambda x, y: x + y, 
+                                    [self.ne_loss(y_) for y_ in self.ae.decoder.classifiers])
+        
+        # reconstruction loss
+        l_rec_u = 0
+        l_rec_u += self.recon_loss(x_u, x_rec) \
+                   + reduce(lambda x, y: x + y,
+                            [self.recon_loss(x, y) for x, y in zip(
+                                self.ae.encoder.hiddens,
+                                self.ae.decoder.hiddens[::-1])])
+
+        # unlabel jsd loss
+        l_ljsd_l = reduce(lambda x, y: x + y,
+                          [self.jsd_loss(x, y) for x, y in zip(
+                              self.ae.encoder.classifiers,
+                              self.ae.decoder.classifiers[::-1])])
+        
+        # loss for unlabeled samples
+        loss_u = l_ne_u + l_rec_u + l_ljd_u
 
         loss = loss_l + loss_u
 
