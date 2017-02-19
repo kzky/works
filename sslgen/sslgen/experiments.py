bb54585522ce1dd34434818b83cf7efdf308d311
kzky <rkzfilter@gmail.com>
2017-02-19 23:31:39 +0900

    Fix.

--- sslgen/sslgen/experiments.py
+++ sslgen/sslgen/experiments.py
@@ -772,7 +772,7 @@ class Experiment005(Experiment000):
         h = self.encoder(x_real)
         x_rec = self.decoder(h, self.encoder.hiddens, y)
         loss_rec = reduce(lambda x, y: x + y, 
-                          [self.recon_loss(h0, 1) for h0, h1 in \ 
+                          [self.recon_loss(h0, 1) for h0, h1 in \
                            zip(self.encoder.hiddens, self.decoder.hiddens[::-1])])
         self.encoder.cleargrads()
         self.decoder.cleargrads()
