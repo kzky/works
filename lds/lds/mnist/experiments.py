8e2c8613653557b153606445caedb9467dc00557
kzky <rkzfilter@gmail.com>
2017-03-11 23:02:50 +0900

    Add experiment030.

--- lds/lds/mnist/experiments.py
+++ lds/lds/mnist/experiments.py
@@ -1383,3 +1383,72 @@ class Experiment029(Experiment025):
         y = self.ae.encoder(x_l, test=True)
         acc = F.accuracy(y, y_l)
         return acc
+
+
+class Experiment030(Experiment025):
+    """Regularize with reconstruction
+
+    """
+    def __init__(self, device=None, learning_rate=1e-3, act=F.relu, lr_decay=False):
+        super(Experiment030, self).__init__(
+            device=device,
+            learning_rate=learning_rate,
+            act=act, 
+        )
+        
+        # Model
+        from lds.mnist.cnn_model_005 import AutoEncoder
+        self.ae = AutoEncoder(act)
+        self.ae.to_gpu(device) if self.device else None
+
+        # Optimizer
+        self.optimizer = optimizers.Adam(learning_rate)
+        self.optimizer.setup(self.ae)
+        self.optimizer.use_cleargrads()
+        
+    def test(self, x_l, y_l):
+        y = self.ae.encoder(x_l, test=True)
+        acc = F.accuracy(y, y_l)
+        return acc
+
+    def train(self, x_l, y_l, x_u):
+        # Labeled samples
+        y = self.ae.encoder(x_l)
+        x_rec = self.ae.decoder(y)
+
+        # cronss entropy loss
+        l_ce_l = 0
+        l_ce_l += F.softmax_cross_entropy(y, y_l)
+
+        # reconstruction loss
+        l_rec_l = 0
+        l_rec_l += self.recon_loss(x_l, x_rec) \
+                   + reduce(lambda x, y: x + y,
+                            [self.recon_loss(x, y) for x, y in zip(
+                                self.ae.encoder.hiddens,
+                                self.ae.decoder.hiddens[::-1])])
+
+        # loss for labeled samples
+        loss_l = l_ce_l + l_rec_l
+
+        # Unlabeled samples
+        y = self.ae.encoder(x_u)
+        x_rec = self.ae.decoder(y)
+
+        # reconstruction loss
+        l_rec_u = 0
+        l_rec_u += self.recon_loss(x_u, x_rec) \
+                   + reduce(lambda x, y: x + y,
+                            [self.recon_loss(x, y) for x, y in zip(
+                                self.ae.encoder.hiddens,
+                                self.ae.decoder.hiddens[::-1])])
+
+        # loss for unlabeled samples
+        loss_u = l_rec_u
+
+        loss = loss_l + loss_u
+
+        # Backward and Update
+        self.ae.cleargrads()
+        loss.backward()
+        self.optimizer.update()
