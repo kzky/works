0bdaff7adb3c2d2ac9df862c7b209dc281642de0
kzky <rkzfilter@gmail.com>
2017-03-12 00:40:21 +0900

    Fix map parameters.

--- lds/lds/mnist/cnn_model_006.py
+++ lds/lds/mnist/cnn_model_006.py
@@ -22,20 +22,20 @@ class Encoder(Chain):
             # Encoder
             conv0=L.Convolution2D(1, 32, 3, stride=1, pad=1),
             conv1=L.Convolution2D(32, 32, 3, stride=1, pad=1),
-            conv2=L.Convolution2D(32, 64, 4, stride=2, pad=1),
-            conv3=L.Convolution2D(64, 64, 3, stride=1, pad=1),
-            conv4=L.Convolution2D(64, 128, 3, stride=1, pad=1),
-            conv5=L.Convolution2D(128, 128, 4, stride=2, pad=1),
-            linear0=L.Linear(128 * 7 * 7, 64),
-            linear1=L.Linear(64, 10),
+            conv2=L.Convolution2D(32, 32, 4, stride=2, pad=1),
+            conv3=L.Convolution2D(32, 64, 3, stride=1, pad=1),
+            conv4=L.Convolution2D(64, 64, 3, stride=1, pad=1),
+            conv5=L.Convolution2D(64, 64, 4, stride=2, pad=1),
+            linear0=L.Linear(64 * 7 * 7, 32),
+            linear1=L.Linear(32, 10),
 
             bn_conv0=L.BatchNormalization(32, decay=0.9, use_cudnn=True),
             bn_conv1=L.BatchNormalization(32, decay=0.9, use_cudnn=True),
-            bn_conv2=L.BatchNormalization(64, decay=0.9, use_cudnn=True),
+            bn_conv2=L.BatchNormalization(32, decay=0.9, use_cudnn=True),
             bn_conv3=L.BatchNormalization(64, decay=0.9, use_cudnn=True),
-            bn_conv4=L.BatchNormalization(128, decay=0.9, use_cudnn=True),
-            bn_conv5=L.BatchNormalization(128, decay=0.9, use_cudnn=True),
-            bn_linear0=L.BatchNormalization(64, decay=0.9, use_cudnn=True),
+            bn_conv4=L.BatchNormalization(64, decay=0.9, use_cudnn=True),
+            bn_conv5=L.BatchNormalization(64, decay=0.9, use_cudnn=True),
+            bn_linear0=L.BatchNormalization(32, decay=0.9, use_cudnn=True),
         )
 
         self.act = act
@@ -58,7 +58,6 @@ class Encoder(Chain):
         h = self.conv2(h)  # 28x28 -> 14x14
         h = self.bn_conv2(h, test)
         h = self.act(h)
-        #self.hiddens.append(h)
 
         h = self.conv3(h)
         h = self.bn_conv3(h, test)
@@ -73,7 +72,6 @@ class Encoder(Chain):
         h = self.conv5(h)  # 14x14 -> 7x7
         h = self.bn_conv5(h)
         h = self.act(h)
-        #self.hiddens.append(h)
 
         # Linear
         h = self.linear0(h)   # 128x7x7 -> 64
@@ -89,20 +87,20 @@ class Decoder(Chain):
     """
     def __init__(self, act=F.relu):
         super(Decoder, self).__init__(
-            linear0=L.Linear(10, 64),
-            linear1=L.Linear(64, 128 * 7 * 7),
-            deconv0=L.Deconvolution2D(128, 128, 2, stride=2, pad=0),
-            deconv1=L.Deconvolution2D(128, 64, 3, stride=1, pad=1),
-            deconv2=L.Deconvolution2D(64, 64, 3, stride=1, pad=1),
-            deconv3=L.Deconvolution2D(64, 32, 2, stride=2, pad=0),
+            linear0=L.Linear(10, 32),
+            linear1=L.Linear(32, 64 * 7 * 7),
+            deconv0=L.Deconvolution2D(64, 64, 2, stride=2, pad=0),
+            deconv1=L.Deconvolution2D(64, 64, 3, stride=1, pad=1),
+            deconv2=L.Deconvolution2D(64, 32, 3, stride=1, pad=1),
+            deconv3=L.Deconvolution2D(32, 32, 2, stride=2, pad=0),
             deconv4=L.Deconvolution2D(32, 32, 3, stride=1, pad=1),
             deconv5=L.Deconvolution2D(32, 1, 3, stride=1, pad=1),
             
-            bn_linear0=L.BatchNormalization(64, decay=0.9, use_cudnn=True),
-            bn_linear1=L.BatchNormalization(128 * 7 * 7, decay=0.9, use_cudnn=True),
-            bn_deconv0=L.BatchNormalization(128, decay=0.9, use_cudnn=True),
+            bn_linear0=L.BatchNormalization(32, decay=0.9, use_cudnn=True),
+            bn_linear1=L.BatchNormalization(64 * 7 * 7, decay=0.9, use_cudnn=True),
+            bn_deconv0=L.BatchNormalization(64, decay=0.9, use_cudnn=True),
             bn_deconv1=L.BatchNormalization(64, decay=0.9, use_cudnn=True),
-            bn_deconv2=L.BatchNormalization(64, decay=0.9, use_cudnn=True),
+            bn_deconv2=L.BatchNormalization(32, decay=0.9, use_cudnn=True),
             bn_deconv3=L.BatchNormalization(32, decay=0.9, use_cudnn=True),
             bn_deconv4=L.BatchNormalization(32, decay=0.9, use_cudnn=True),
         )
@@ -124,13 +122,13 @@ class Decoder(Chain):
         h = self.act(h)
         bs = h.shape[0]
         d = h.shape[1]
-        h = F.reshape(h, (bs, 128, 7, 7))
+        h = F.reshape(h, (bs, 64, 7, 7))
+        self.hiddens.append(h)
 
         # Deconvolution
         h = self.deconv0(h)
         h = self.bn_deconv0(h, test)  # 7x7 -> 14x14
         h = self.act(h)
-        self.hiddens.append(h)
 
         h = self.deconv1(h)
         h = self.bn_deconv1(h, test)
