0da0ab519ecc75fc81a98febac3a403793422ab6
kzky <rkzfilter@gmail.com>
2017-03-18 17:22:15 +0900

    Fix.

--- lds/lds/svhn/datasets.py
+++ lds/lds/svhn/datasets.py
@@ -161,7 +161,7 @@ class Separator(object):
             fpath="/home/kzk/.chainer/dataset/pfnet/chainer/svhn/train_32x32.mat"):
         
         data = scipy.io.loadmat(fpath)
-        n = len(data["X"].shape[-1])
+        n = data["X"].shape[-1]
         idxs = np.arange(n)
         idxs_l = self._sample_indices(np.squeeze(data["y"]))
         idxs_u = np.asarray(list(set(idxs) - set(idxs_l)))
