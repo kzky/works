932e444e5b3310e085138143af9d7b67772b091c
kzky <rkzfilter@gmail.com>
2017-03-19 22:58:25 +0900

    Add components to be saved.

--- lds/scripts/zca_whitening_cifar10.py
+++ lds/scripts/zca_whitening_cifar10.py
@@ -9,7 +9,7 @@ def main():
 
     # input/output paths
     fpath_inp = os.path.join(home, "datasets/cifar10/cifar-10.npz")
-    fpath_out = os.path.join(home, "datasets/cifar10/zca_matrix.npz") # Z; ZCA whitening matrix saved
+    fpath_out = os.path.join(home, "datasets/cifar10/zca_components.npz") # Z; ZCA whitening matrix saved
 
     # Pre setting
     X = np.load(fpath_inp)["train_x"]  # n x d matrix of cifar 10 training samples
@@ -17,7 +17,7 @@ def main():
     D = X.shape[1]
     X = X / 255.
     x_mean = np.mean(X, axis=0)
-    X -= x_mean
+    X -= X_mean
     
     # Eigen value decomposition
     C = np.dot(X.T, X) / N
@@ -32,7 +32,7 @@ def main():
     W_zca = np.dot(U/sqlam[np.newaxis, :], U.T)
 
     # Save
-    np.save(fpath_out, {"W_zca": W_zca})
+    np.save(fpath_out, {"W_zca": W_zca, "X_mean": X_mean})
 
 
 if __name__ == '__main__':
