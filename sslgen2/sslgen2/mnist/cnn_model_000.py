b449e9d5196e1478a8c14758f6e4303d9da5425b
kzky <rkzfilter@gmail.com>
2017-03-20 16:41:21 +0900

    Fix outuput dim.

--- sslgen2/sslgen2/mnist/cnn_model_000.py
+++ sslgen2/sslgen2/mnist/cnn_model_000.py
@@ -115,7 +115,7 @@ class Discriminator(Chain):
         super(Discriminator, self).__init__(
             convunit0=ConvUnit(1, 64, k=4, s=2, p=1, act=act),
             convunit1=ConvUnit(64, 128, k=4, s=2, p=1, act=act),
-            linear=L.Linear(128, 10), 
+            linear=L.Linear(128, 1), 
         )
         self.act= act
 
