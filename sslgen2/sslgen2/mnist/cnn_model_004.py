ffed107017896999cc3a8cf650172ee6978c3eb2
kzky <rkzfilter@gmail.com>
2017-03-22 23:17:46 +0900

    Add experiment011.

---
+++ sslgen2/sslgen2/mnist/cnn_model_004.py
@@ -0,0 +1,132 @@
+"""Models
+"""
+import numpy as np
+import chainer
+import chainer.variable as variable
+from chainer.functions.activation import lstm
+from chainer import cuda, Function, gradient_check, report, training, utils, Variable
+from chainer import datasets, iterators, optimizers, serializers
+from chainer import Link, Chain, ChainList
+import chainer.functions as F
+import chainer.links as L
+from collections import OrderedDict
+import logging
+import time
+from sslgen2.utils import to_device
+
+class ConvUnit(Chain):
+    def __init__(self, imap, omap, k=4, s=2, p=1, act=F.relu):
+        super(ConvUnit, self).__init__(
+            conv=L.Convolution2D(imap, omap, ksize=k, stride=s, pad=p, ),
+            bn=L.BatchNormalization(omap, decay=0.9, use_cudnn=True),
+        )
+        self.act = act
+        
+    def __call__(self, h, test=False):
+        h = self.conv(h)
+        h = self.bn(h, test)
+        h = self.act(h)
+        return h
+
+class DeconvUnit(Chain):
+    def __init__(self, imap, omap, k=4, s=2, p=1, act=F.relu):
+        super(DeconvUnit, self).__init__(
+            deconv=L.Deconvolution2D(imap, omap, ksize=k, stride=s, pad=p, ),
+            bn=L.BatchNormalization(omap, decay=0.9, use_cudnn=True),
+        )
+        self.act = act
+        
+    def __call__(self, h, test=False):
+        h = self.deconv(h)
+        h = self.bn(h, test)
+        h = self.act(h)
+        return h
+        
+class Encoder(Chain):
+
+    def __init__(self, device=None, act=F.relu):
+        super(Encoder, self).__init__(
+            convunit0=ConvUnit(1, 64, k=4, s=2, p=1, act=act),
+            convunit1=ConvUnit(64, 128, k=4, s=2, p=1, act=act),
+        )
+        
+    def __call__(self, x, test=False):
+        h = self.convunit0(x, test)
+        h = self.convunit1(h, test)
+        return h
+
+class Decoder(Chain):
+
+    def __init__(self, device=None, act=F.relu):
+        super(Decoder, self).__init__(
+            deconvunit0=DeconvUnit(128, 64, k=4, s=2, p=1, act=act),
+            deconv=L.Deconvolution2D(64, 1, ksize=4, stride=2, pad=1, ),
+        )
+        self.act= act
+
+    def __call__(self, x, test=False):
+        h = self.deconvunit0(x, test)
+        h = self.deconv(h)
+        h = F.tanh(h)
+        return h
+
+class Generator0(Chain):
+
+    def __init__(self, omap, h, w, dim=100, device=None, act=F.relu,):
+        super(Generator0, self).__init__(
+            linear=L.Linear(dim, omap*h*w),
+            bn=L.BatchNormalization(omap*h*w, use_cudnn=True)
+        )
+        self.act = act
+        self.omap = omap
+        self.h = h
+        self.w = w
+        
+    def __call__(self, z, test=False):
+        h = self.linear(z)
+        h = self.bn(h, test)
+        h = F.reshape(h, (h.shape[0], self.omap, self.h, self.w))
+        h = self.act(h)
+        return h
+
+class Generator(Chain):
+
+    def __init__(self, device=None, act=F.relu ,dim=100):
+        super(Generator, self).__init__(
+            generator0=Generator0(128, 7, 7, dim=dim+128, device=device, act=act),
+            deconvunit0=DeconvUnit(256, 128, k=1, s=1, p=0, act=act),
+            deconvunit1=DeconvUnit(128, 64, k=4, s=2, p=1, act=act),
+            deconv=L.Deconvolution2D(64, 1, ksize=4, stride=2, pad=1),
+        )
+        self.act= act
+
+    def __call__(self, h_feat, z, test=False):
+        h_feat = F.average_pooling_2d(h_feat, (7, 7))
+        z = F.concat((h_feat, z))
+        h = self.generator0(z, test)
+        h = self.deconvunit0(h, test)
+        h = self.deconvunit1(h, test)
+        h = self.deconv(h)
+        h = F.tanh(h)
+        return h
+
+class Discriminator(Chain):
+
+    def __init__(self, device=None, act=F.relu):
+        super(Discriminator, self).__init__(
+            convunit0=ConvUnit(1, 64, k=4, s=2, p=1, act=act),
+            convunit1=ConvUnit(64, 128, k=4, s=2, p=1, act=act),
+            linear=L.Linear(128*2, 1), 
+        )
+        self.act= act
+
+    def __call__(self, x, h_feat, test=False):
+        h = self.convunit0(x, test)
+        h = self.convunit1(h, test)
+        h = F.average_pooling_2d(h, (7, 7))
+        h_feat = F.average_pooling_2d(h_feat, (7, 7))
+        h = F.concat((h, h_feat))
+        h = self.linear(h)
+        #h = F.sigmoid(h)
+        return h
+    
--- sslgen2/sslgen2/mnist/experiments.py
+++ sslgen2/sslgen2/mnist/experiments.py
@@ -1081,3 +1081,90 @@ class Experiment010(Experiment002):
         x_gen = self.decoder(self.generator0(z, test), test)
         return x_gen
     
+
+class Experiment011(Experiment008):
+    """Enc-Dec, Enc-Gen-Enc, Enc-Gen-Dis.
+
+    - Discriminator is conditioned on hidden feature of x_real averaged over 
+    spatial size
+    """
+    def __init__(self, device=None, learning_rate=1e-3, act=F.relu, dim=100):
+        # Settings
+        self.device = device
+        self.act = act
+        self.learning_rate = learning_rate
+        self.dim = dim
+
+        # Losses
+        self.recon_loss = ReconstructionLoss()
+        self.lsgan_loss = LSGANLoss()
+
+        # Model
+        from sslgen2.mnist.cnn_model_002 \
+            import Encoder, Decoder, Generator, Discriminator
+        self.encoder = Encoder(device, act)
+        self.decoder = Decoder(device, act)
+        self.generator = Generator(device, act ,dim)
+        self.discriminator = Discriminator(device, act)
+
+        self.encoder.to_gpu(device) if self.device else None
+        self.decoder.to_gpu(device) if self.device else None
+        self.generator.to_gpu(device) if self.device else None
+        self.discriminator.to_gpu(device) if self.device else None
+        
+        # Optimizer
+        self.optimizer_enc = optimizers.Adam(learning_rate, beta1=0.5)
+        self.optimizer_enc.setup(self.encoder)
+        self.optimizer_enc.use_cleargrads()
+        self.optimizer_dec = optimizers.Adam(learning_rate, beta1=0.5)
+        self.optimizer_dec.setup(self.decoder)
+        self.optimizer_dec.use_cleargrads()
+        self.optimizer_gen = optimizers.Adam(learning_rate, beta1=0.5)
+        self.optimizer_gen.setup(self.generator)
+        self.optimizer_gen.use_cleargrads()
+        self.optimizer_dis = optimizers.Adam(learning_rate, beta1=0.5)
+        self.optimizer_dis.setup(self.discriminator)
+        self.optimizer_dis.use_cleargrads()
+
+    def train(self, x):
+        # Encoder/Decoder
+        h = self.encoder(x)
+        x_rec = self.decoder(h)
+        l_rec = self.recon_loss(x, x_rec)
+        self.cleargrads()
+        l_rec.backward()
+        self.optimizer_enc.update()
+        self.optimizer_dec.update()
+
+        # Discriminator
+        h = Variable(h.data)  # disconnect
+        xp = cuda.get_array_module(x)
+        z = Variable(cuda.to_gpu(xp.random.rand(x.shape[0], self.dim).astype(xp.float32), self.device))
+        x_gen = self.generator(h, z)
+        d_x_gen = self.discriminator(x_gen, h)
+        d_x_real = self.discriminator(x, h)
+        l_dis = self.lsgan_loss(d_x_gen, d_x_real)
+        self.cleargrads()
+        l_dis.backward()
+        self.optimizer_dis.update()
+        
+        # Generator
+        xp = cuda.get_array_module(x)
+        z = Variable(cuda.to_gpu(xp.random.rand(x.shape[0], self.dim).astype(xp.float32), self.device))
+        x_gen = self.generator(h, z)
+        d_x_gen = self.discriminator(x_gen, h)
+        h_gen = self.encoder(x_gen)
+        l_gen = self.lsgan_loss(d_x_gen)
+        self.cleargrads()
+        l_gen.backward()
+        self.optimizer_gen.update()
+
+    def test(self, x_l, y_l, epoch, filename):
+        """generate samples, then save"""
+        x_gen = self.generate(x_l, test=True)
+        self.save(x_l, x_gen, epoch, filename)
+        h = self.encoder(x_l, test=True)
+        d_x_gen = self.discriminator(x_gen, h,  test=True)
+        loss = self.lsgan_loss(d_x_gen)
+        return loss
+    
