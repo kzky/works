059b3f9f3c5344e5af65288bd1d9ff226e22ca8c
kzky <rkzfilter@gmail.com>
2017-04-01 18:25:32 +0900

    Add exp001.

--- sslgen3/sslgen3/svhn/experiments.py
+++ sslgen3/sslgen3/svhn/experiments.py
@@ -22,6 +22,7 @@ from sklearn.metrics import confusion_matrix
 class Experiment000(object):
     """Enc-MLP-Dec-Dis
 
+    - Spatial size is reduce by strided convolution.
     """
     def __init__(self, device=None, learning_rate=1e-3, act=F.relu, n_cls=10):
         # Settings
@@ -132,3 +133,68 @@ class Experiment000(object):
         y = cuda.to_gpu(h, self.device)
         return Variable(y)
         
+
+class Experiment001(Experiment000):
+    """Enc-MLP-Dec-Dis
+
+    - Spatial size is reduce by strided convolution.
+    """
+    def __init__(self, device=None, learning_rate=1e-3, act=F.relu, n_cls=10):
+        # Settings
+        self.device = device
+        self.act = act
+        self.learning_rate = learning_rate
+        self.n_cls = n_cls
+
+        # Losses
+        self.recon_loss = ReconstructionLoss()
+        self.gan_loss = GANLoss()
+        self.er_loss = EntropyRegularizationLoss()
+
+        # Model
+        from sslgen3.svhn.cnn_model_001 \
+            import Encoder, MLP, Decoder, Discriminator
+        self.encoder = Encoder(device, act)
+        self.mlp = MLP(device, act)
+        self.decoder = Decoder(device, act)
+        self.discriminator = Discriminator(device, act, n_cls)
+        self.encoder.to_gpu(device) if self.device else None
+        self.mlp.to_gpu(device) if self.device else None
+        self.decoder.to_gpu(device) if self.device else None
+        self.discriminator.to_gpu(device) if self.device else None
+        
+        # Optimizer
+        self.optimizer_enc = optimizers.Adam(learning_rate)
+        self.optimizer_enc.setup(self.encoder)
+        self.optimizer_enc.use_cleargrads()
+        self.optimizer_mlp = optimizers.Adam(learning_rate)
+        self.optimizer_mlp.setup(self.mlp)
+        self.optimizer_mlp.use_cleargrads()
+
+    def train(self, x_l, y, x_u):
+        self._train(x_l, (x_l, y), y)
+        self._train(x_u, (x_l, y))
+        
+    def _train(self, x, xy, y_0=None):
+        x_, y_ = xy
+        
+        # Encoder/Decoder
+        h = self.encoder(x)
+        y_pred = self.mlp(h)
+
+        loss = 0
+        loss += self.er_loss(y_pred)   # ER loss
+        if y_0 is not None:
+            loss += F.softmax_cross_entropy(y_pred, y_0)  # CE loss
+
+        x_rec = self.decoder(h)
+        loss += self.recon_loss(x, x_rec) \
+                + reduce(lambda u, v: u + v,
+                         [self.recon_loss(u, v) \
+                          for u, v in zip(self.encoder.hiddens,
+                                          self.decoder.hiddens[::-1])])  # RC loss
+        self.cleargrads()
+        loss.backward()
+        self.optimizer_enc.update()
+        self.optimizer_dec.update()
+        self.optimizer_mlp.update()
