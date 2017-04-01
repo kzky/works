a182713b74240eb5887cc72c3cd22ada8fbbfc66
kzky <rkzfilter@gmail.com>
2017-04-01 18:50:09 +0900

    Add svhn with st.

--- st/st/svhn/cnn_model_000.py
+++ st/st/svhn/cnn_model_000.py
@@ -14,6 +14,7 @@ import logging
 import time
 from st.utils import to_device
 
+
 class ConvUnit(Chain):
     def __init__(self, imap, omap, k=4, s=2, p=1, act=F.relu):
         super(ConvUnit, self).__init__(
@@ -28,93 +29,70 @@ class ConvUnit(Chain):
         h = self.act(h)
         return h
 
-class DeconvUnit(Chain):
-
-    def __init__(self, imap, omap, k=4, s=2, p=1, act=F.relu):
-        super(DeconvUnit, self).__init__(
-            deconv=L.Deconvolution2D(imap, omap, ksize=k, stride=s, pad=p, ),
-            bn=L.BatchNormalization(omap, decay=0.9, use_cudnn=True),
+class ResConvUnit(Chain):
+    def __init__(self, imap, omap, act=F.relu):
+        super(ResConvUnit, self).__init__(
+            conv0=L.Convolution2D(imap, omap, ksize=1, stride=1, pad=0, ),
+            bn0=L.BatchNormalization(omap, decay=0.9, use_cudnn=True),
+            conv1=L.Convolution2D(imap, omap, ksize=3, stride=1, pad=1, ),
+            bn1=L.BatchNormalization(omap, decay=0.9, use_cudnn=True),
+            conv2=L.Convolution2D(imap, omap, ksize=1, stride=1, pad=0, ),
+            bn2=L.BatchNormalization(omap, decay=0.9, use_cudnn=True),
         )
         self.act = act
         
-    def __call__(self, h, test=False):
-        h = self.deconv(h)
-        h = self.bn(h, test)
+    def __call__(self, x, test=False):
+        h = self.conv0(x)
+        h = self.bn0(h, test)
         h = self.act(h)
+
+        h = self.conv1(h)
+        h = self.bn1(h, test)
+        h = self.act(h)
+
+        h = self.conv2(h)
+        h = h + x
+        h = self.bn2(h, test)
+        h = self.act(h)
+
         return h
-        
-class Encoder(Chain):
+
+class Model(Chain):
 
     def __init__(self, device=None, act=F.relu):
-        super(Encoder, self).__init__(
-            convunit0=ConvUnit(3, 64, k=4, s=2, p=1, act=act),
-            convunit1=ConvUnit(64, 128, k=4, s=2, p=1, act=act),
-            convunit2=ConvUnit(128, 256, k=4, s=2, p=1, act=act),
+        super(Model, self).__init__(
+            convunit=ConvUnit(3, 64, k=3, s=1, p=1, act=act),
+            resconvunit0=ResConvUnit(64, 64),
+            resconvunit1=ResConvUnit(64, 64),
+            resconvunit2=ResConvUnit(64, 64),
+            resconvunit3=ResConvUnit(64, 64),
+            linear=L.Linear(64*7*7, 10),
         )
-        self.hiddens = []
         self.act = act
+        self.hiddens = []
         
     def __call__(self, x, test=False):
         self.hiddens = []
 
-        h = self.convunit0(x, test)
+        h = self.convunit(x, test)
+        h = F.dropout(h, train=not test)
+
+        h = self.resconvunit0(h, test)
         self.hiddens.append(h)
-        h = self.convunit1(h, test)
+        h = F.dropout(h, train=not test)
+        h = self.resconvunit1(h, test)
         self.hiddens.append(h)
-        h = self.convunit2(h, test)
-        return h
+        h = F.max_pooling_2d(h, (2, 2))
+        h = F.dropout(h, train=not test)
 
-class MLP(Chain):
-    def __init__(self, device=None, act=F.relu):
-        super(MLP, self).__init__(
-            linear=L.Linear(256*4*4, 10)
-        )
-
-    def __call__(self, h):
-        y = self.linear(h)
-        return y
-
-class Decoder(Chain):
-
-    def __init__(self, device=None, act=F.relu):
-        super(Decoder, self).__init__(
-            deconvunit0=DeconvUnit(256, 128, k=4, s=2, p=1, act=act),
-            deconvunit1=DeconvUnit(128, 64, k=4, s=2, p=1, act=act),
-            deconv=L.Deconvolution2D(64, 3, ksize=4, stride=2, pad=1, ),
-        )
-        self.act= act
-        self.hiddens = []
-
-    def __call__(self, h, test=False):
-        self.hiddens = []
-
-        h = self.deconvunit0(h, test)
+        h = self.resconvunit2(h, test)
         self.hiddens.append(h)
-        h = self.deconvunit1(h, test)
+        h = F.dropout(h, train=not test)
+        h = self.resconvunit3(h, test)
         self.hiddens.append(h)
-        h = self.deconv(h)
-        h = F.tanh(h)
-        return h
+        h = F.max_pooling_2d(h, (2, 2))
+        h = F.dropout(h, train=not test)
 
-class Discriminator(Chain):
-
-    def __init__(self, device=None, act=F.relu, n_cls=10):
-        super(Discriminator, self).__init__(
-            convunit0=ConvUnit(3, 64, k=4, s=2, p=1, act=act),
-            convunit1=ConvUnit(64, 128, k=4, s=2, p=1, act=act),
-            convunit2=ConvUnit(128, 256, k=4, s=2, p=1, act=act),
-            linear=L.Linear(256*4*4 + n_cls, 1), 
-        )
-        self.act= act
-
-    def __call__(self, x, y, test=False):
-        h = self.convunit0(x, test)
-        h = self.convunit1(h, test)
-        h = self.convunit2(h, test)
-        shape = h.shape
-        h = F.reshape(h, (shape[0], np.prod(shape[1:])))
-        h = F.concat((h, y))
         h = self.linear(h)
-        h = F.sigmoid(h)
         return h
-    
+
