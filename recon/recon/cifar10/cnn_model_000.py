0fc1f00f240d72000f99ccb594ca6928e31bfb74
kzky <rkzfilter@gmail.com>
2017-04-08 15:41:56 +0900

    Add cnn model 000 for cifar10.

--- recon/recon/cifar10/cnn_model_000.py
+++ recon/recon/cifar10/cnn_model_000.py
@@ -28,6 +28,18 @@ class ConvUnit(Chain):
         h = self.act(h)
         return h
 
+class ResConvUnit(Chain):
+    def __init__(self, imap, omap, act=F.relu):
+        super(ResConvUnit, self).__init__(
+            conv0=L.Convolution2D(imap, omap, ksize=1, stride=1, pad=0, ),
+            bn0=L.BatchNormalization(omap, decay=0.9, use_cudnn=True),
+            conv1=L.Convolution2D(imap, omap, ksize=3, stride=1, pad=1, ),
+            bn1=L.BatchNormalization(omap, decay=0.9, use_cudnn=True),
+            conv2=L.Convolution2D(imap, omap, ksize=1, stride=1, pad=0, ),
+            bn2=L.BatchNormalization(omap, decay=0.9, use_cudnn=True),
+        )
+        self.act = act
+
 class DeconvUnit(Chain):
 
     def __init__(self, imap, omap, k=4, s=2, p=1, act=F.relu):
@@ -47,9 +59,11 @@ class Encoder(Chain):
 
     def __init__(self, device=None, act=F.relu):
         super(Encoder, self).__init__(
-            convunit0=ConvUnit(3, 64, k=4, s=2, p=1, act=act),
-            convunit1=ConvUnit(64, 128, k=4, s=2, p=1, act=act),
-            convunit2=ConvUnit(128, 256, k=4, s=2, p=1, act=act),
+            convunit0=ConvUnit(3, 64, k=3, s=1, p=1, act=act),
+            convunit1=ConvUnit(64, 128, k=3, s=1, p=1, act=act),
+            convunit2=ConvUnit(128, 256, k=3, s=1, p=1, act=act),
+            linear=L.Linear(256*7*7, 128),
+            bn=L.BatchNormalization(128, decay=0.9, use_cudnn=True),
         )
         self.hiddens = []
         self.act = act
@@ -58,16 +72,22 @@ class Encoder(Chain):
         self.hiddens = []
 
         h = self.convunit0(x, test)
+        h = F.max_pooling_2d(h, (2, 2))
         self.hiddens.append(h)
         h = self.convunit1(h, test)
+        h = F.max_pooling_2d(h, (2, 2))
         self.hiddens.append(h)
         h = self.convunit2(h, test)
+        h = F.max_pooling_2d(h, (2, 2))
+        self.hiddens.append(h)
+        h = self.linear(h)
+        h = self.bn(h)
         return h
 
 class MLP(Chain):
     def __init__(self, device=None, act=F.relu):
         super(MLP, self).__init__(
-            linear=L.Linear(256*4*4, 10)
+            linear=L.Linear(128, 10),
         )
 
     def __call__(self, h):
@@ -78,6 +98,8 @@ class Decoder(Chain):
 
     def __init__(self, device=None, act=F.relu):
         super(Decoder, self).__init__(
+            linear=L.Linear(128, 256*7*7),
+            bn=L.BatchNormalization(256*7*7, decay=0.9, use_cudnn=True),
             deconvunit0=DeconvUnit(256, 128, k=4, s=2, p=1, act=act),
             deconvunit1=DeconvUnit(128, 64, k=4, s=2, p=1, act=act),
             deconv=L.Deconvolution2D(64, 3, ksize=4, stride=2, pad=1, ),
@@ -87,7 +109,10 @@ class Decoder(Chain):
 
     def __call__(self, h, test=False):
         self.hiddens = []
-
+        h = self.linear(h)
+        h = self.bn(h)
+        h = F.reshape(h, (h.shape[0], 256, 7, 7))
+        self.hiddens.append(h)
         h = self.deconvunit0(h, test)
         self.hiddens.append(h)
         h = self.deconvunit1(h, test)
@@ -96,25 +121,3 @@ class Decoder(Chain):
         h = F.tanh(h)
         return h
 
-class Discriminator(Chain):
-
-    def __init__(self, device=None, act=F.relu, n_cls=10):
-        super(Discriminator, self).__init__(
-            convunit0=ConvUnit(3, 64, k=4, s=2, p=1, act=act),
-            convunit1=ConvUnit(64, 128, k=4, s=2, p=1, act=act),
-            convunit2=ConvUnit(128, 256, k=4, s=2, p=1, act=act),
-            linear=L.Linear(256*4*4 + n_cls, 1), 
-        )
-        self.act= act
-
-    def __call__(self, x, y, test=False):
-        h = self.convunit0(x, test)
-        h = self.convunit1(h, test)
-        h = self.convunit2(h, test)
-        shape = h.shape
-        h = F.reshape(h, (shape[0], np.prod(shape[1:])))
-        h = F.concat((h, y))
-        h = self.linear(h)
-        h = F.sigmoid(h)
-        return h
-    
