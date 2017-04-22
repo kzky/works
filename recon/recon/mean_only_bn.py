6e88abade25869d41d03eb40e1d9e0ec92245fc5
kzky <rkzfilter@gmail.com>
2017-04-22 18:00:40 +0900

    Fix.

--- recon/recon/mean_only_bn.py
+++ recon/recon/mean_only_bn.py
@@ -1,10 +1,10 @@
 import numpy
 
+from chainer import cuda
 from chainer.functions.normalization import batch_normalization
 from chainer import initializers
 from chainer import link
 from chainer import variable
-from chainer import cuda
 
 class BatchNormalization(link.Link):
 
@@ -42,7 +42,7 @@ class BatchNormalization(link.Link):
         use_cudnn (bool): If ``True``, then this link uses cuDNN if available.
 
     See: `Batch Normalization: Accelerating Deep Network Training by Reducing\
-          Internal Covariate Shift <http://arxiv.org/abs/1502.03167>`_
+          Internal Covariate Shift <https://arxiv.org/abs/1502.03167>`_
 
     .. seealso::
        :func:`~chainer.functions.batch_normalization`,
@@ -63,7 +63,7 @@ class BatchNormalization(link.Link):
 
     def __init__(self, size, decay=0.9, eps=2e-5, dtype=numpy.float32,
                  use_gamma=True, use_beta=True,
-                 initial_gamma=None, initial_beta=None, use_cudnn=True, device=None):
+                 initial_gamma=None, initial_beta=None, use_cudnn=True):
         super(BatchNormalization, self).__init__()
         if use_gamma:
             self.add_param('gamma', size, dtype=dtype)
@@ -81,7 +81,6 @@ class BatchNormalization(link.Link):
         self.decay = decay
         self.eps = eps
         self.use_cudnn = use_cudnn
-        self.device = device
 
     def __call__(self, x, test=False, finetune=False):
         """Invokes the forward propagation of BatchNormalization.
@@ -104,27 +103,23 @@ class BatchNormalization(link.Link):
         during training, and normalizes the input using batch statistics.
 
         """
-
-        # avg_var is always one
-        self.one_var = self.xp.ones(self.avg_mean.shape, dtype=x.dtype)
-
         if hasattr(self, 'gamma'):
             gamma = self.gamma
         else:
-            gamma_ = self.xp.ones(self.avg_mean.shape, dtype=x.dtype)
-            if self.device is None:
-                gamma = variable.Variable(gamma_, volatile='auto')
-            else:
-                gamma = \
-                        variable.Variable(cuda.to_gpu(gamma_, self.device), volatile='auto')
+            with cuda.get_device(self._device_id):
+                gamma = variable.Variable(self.xp.ones(
+                    self.avg_mean.shape, dtype=x.dtype), volatile='auto')
         if hasattr(self, 'beta'):
             beta = self.beta
         else:
-            beta_ = self.xp.zeros(self.avg_mean.shape, dtype=x.dtype)
-            if self.device is None:
-                beta = variable.Variable(beta_, volatile='auto')
-            else:
-                beta = variable.Variable(cuda.to_gpu(beta_, self.device), volatile='auto')
+            with cuda.get_device(self._device_id):
+                beta = variable.Variable(self.xp.zeros(
+                    self.avg_mean.shape, dtype=x.dtype), volatile='auto')
+
+        # Var is always ones
+        with cuda.get_device(self._device_id):
+            self.one_var = variable.Variable(self.xp.ones(
+                self.avg_mean.shape, dtype=x.dtype), volatile='auto')
 
         if not test:
             if finetune:
@@ -144,7 +139,7 @@ class BatchNormalization(link.Link):
             # Use running average statistics or fine-tuned statistics.
             mean = variable.Variable(self.avg_mean, volatile='auto')
             #var = variable.Variable(self.avg_var, volatile='auto')
-            var = variable.Variable(self.one_var, volatile='auto')
+            self.one_var = variable.Variable(self.one_var, volatile='auto')
             ret = batch_normalization.fixed_batch_normalization(
                 x, gamma, beta, mean, var, self.eps, self.use_cudnn)
         return ret
