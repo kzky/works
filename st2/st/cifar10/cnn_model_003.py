4321bda1bdc59372d9358d2769ca4b1edebe04d0
kzky <rkzfilter@gmail.com>
2017-07-01 21:43:15 +0900

    Add st2.

---
+++ st2/st/cifar10/cnn_model_003.py
@@ -0,0 +1,49 @@
+import nnabla as nn
+import nnabla.functions as F
+import nnabla.parametric_functions as PF
+from nnabla.contrib.context import extension_context
+import numpy as np
+
+def conv_unit(x, scope, maps, k=4, s=2, p=1, act=F.relu, test=False):
+    with nn.parameter_scope(scope):
+        h = PF.convolution(x, maps, kernel=(k, k), stride=(s, s), pad=(p, p))
+        h = F.batch_normalization(h, batch_stat=test)
+        h = act(h)
+        return h
+
+def cnn_model_003(ctx, x, act=F.relu, test=False):
+    with nn.context_scope(ctx):
+        # Convblock0
+        h = conv_unit(x, "conv00", 128, k=3, s=1, p=1, act=act, test=test)
+        h = conv_unit(h, "conv01", 128, k=3, s=1, p=1, act=act, test=test)
+        h = conv_unit(h, "conv02", 128, k=3, s=1, p=1, act=act, test=test)
+        h = F.max_pooling(h, (2, 2))  # 32 -> 16
+        with nn.parameter_scope("bn0"):
+            h = F.batch_normalization(h, batch_stat=test)
+        if not test:
+            h = F.dropout(h)
+
+        # Convblock 1
+        h = conv_unit(h, "conv10", 256, k=3, s=1, p=1, act=act, test=test)
+        h = conv_unit(h, "conv11", 256, k=3, s=1, p=1, act=act, test=test)
+        h = conv_unit(h, "conv12", 256, k=3, s=1, p=1, act=act, test=test)
+        h = F.max_pooling(h, (2, 2))  # 16 -> 18
+        with nn.parameter_scope("bn1"):
+            h = F.batch_normalization(h, batch_stat=test)
+        if not test:
+            h = F.dropout(h)
+
+        # Convblock 2
+        h = conv_unit(h, "conv20", 512, k=3, s=1, p=0, act=act, test=test)  # 8 -> 6
+        h = conv_unit(h, "conv21", 256, k=1, s=1, p=0, act=act, test=test)
+        h = conv_unit(h, "conv22", 128, k=1, s=1, p=0, act=act, test=test)
+        h = conv_unit(h, "conv23", 10, k=1, s=1, p=0, act=act, test=test)
+
+        # Convblock 3
+        h = F.average_pooling(h, (6, 6))
+        with nn.parameter_scope("bn2"):
+            h = F.batch_normalization(h, batch_stat=test)
+        h = F.reshape(h, (h.shape[0], np.prod(h.shape[1:])))
+        return h
+
+
---
+++ st2/st/svhn/datasets.py
@@ -0,0 +1,255 @@
+import numpy as np
+import scipy.io
+import os
+from chainer import cuda
+import cv2
+
+class SVHNDataReader(object):
+    """DataReader
+    """
+    def __init__(
+            self,
+            l_train_path=\
+            "/home/kzk/.chainer/dataset/pfnet/chainer/svhn/l_train.mat",
+            u_train_path=\
+            "/home/kzk/.chainer/dataset/pfnet/chainer/svhn/u_train.mat", 
+            test_path=\
+            "/home/kzk/.chainer/dataset/pfnet/chainer/svhn/test.mat",
+            batch_size=64,
+            n_cls=10,
+            da=False,
+            shape=False,
+    ):
+            
+        _l_train_data = dict(scipy.io.loadmat(l_train_path))
+        self.l_train_data = {
+            "X":_l_train_data["X"], 
+            "y":np.squeeze(_l_train_data["y"])}
+        _u_train_data = scipy.io.loadmat(u_train_path)
+        self.u_train_data = {
+            "X":_u_train_data["X"], 
+            "y":np.squeeze(_u_train_data["y"])}
+        _test_data = scipy.io.loadmat(test_path)
+        self.test_data = {
+            "X": _test_data["X"],
+            "y": np.squeeze(_test_data["y"])}
+
+        self._batch_size = batch_size
+        self._next_position_l_train = 0
+        self._next_position_u_train = 0
+
+        self._n_l_train_data = len(self.l_train_data["X"])
+        self._n_u_train_data = len(self.u_train_data["X"])
+        self._n_test_data = len(self.test_data["X"])
+        self._n_cls = n_cls
+        self._da = da
+        self._shape = shape
+
+        print("Num. of labeled samples {}".format(self._n_l_train_data))
+        print("Num. of unlabeled samples {}".format(self._n_u_train_data))
+        print("Num. of test samples {}".format(self._n_test_data))
+        print("Num. of classes {}".format(self._n_cls))
+
+    def get_l_train_batch(self,):
+        """Return next batch data.
+
+        Return next batch data. Once all samples are read, permutate all samples.
+        
+        Returns
+        ------------
+        tuple of 2: First is for sample and the second is for label.
+                            First data is binarized if a value is greater than 0, then 1;
+                            otherwise 0.
+        """
+        # Read data
+        beg = self._next_position_l_train
+        end = self._next_position_l_train+self._batch_size
+        batch_data_x_ = self.l_train_data["X"][beg:end, :]
+        batch_data_y_ = self.l_train_data["y"][beg:end]
+        batch_data_x = ((batch_data_x_ - 127.5)/ 127.5).astype(np.float32)
+
+        batch_data_x0 = self._transform(batch_data_x)
+        batch_data_x1 = self._transform(batch_data_x)
+        batch_data_y = batch_data_y_.astype(np.int32)
+
+        # Reset pointer
+        self._next_position_l_train += self._batch_size
+        if self._next_position_l_train >= self._n_l_train_data:
+            self._next_position_l_train = 0
+
+            # Shuffle
+            idx = np.arange(self._n_l_train_data)
+            np.random.shuffle(idx)
+            self.l_train_data["X"] = self.l_train_data["X"][idx]
+            self.l_train_data["y"] = self.l_train_data["y"][idx]
+
+        return batch_data_x0, batch_data_x1, batch_data_y
+
+    def get_u_train_batch(self,):
+        """Return next batch data.
+
+        Return next batch data. Once all samples are read, permutate all samples.
+
+        Returns:
+        tuple of 2: First is for sample and the second is for label.
+                            First data is binarized if a value is greater than 0, then 1;
+                            otherwise 0.
+        """
+        # Read data
+        beg = self._next_position_u_train
+        end = self._next_position_u_train+self._batch_size
+        batch_data_x_ = self.u_train_data["X"][beg:end, :]
+        batch_data_y_ = self.u_train_data["y"][beg:end]
+        batch_data_x = ((batch_data_x_ - 127.5)/ 127.5).astype(np.float32)
+
+        batch_data_x0 = self._transform(batch_data_x)
+        batch_data_x1 = self._transform(batch_data_x)
+        batch_data_y = batch_data_y_.astype(np.int32)
+
+        # Reset pointer
+        self._next_position_u_train += self._batch_size
+        if self._next_position_u_train >= self._n_u_train_data:
+            self._next_position_u_train = 0
+
+            # Shuffle
+            idx = np.arange(self._n_u_train_data)
+            np.random.shuffle(idx)
+            self.u_train_data["X"] = self.u_train_data["X"][idx]
+            self.u_train_data["y"] = self.u_train_data["y"][idx]
+
+        return batch_data_x0, batch_data_x1, batch_data_y
+
+    def get_test_batch(self,):
+        """Return next batch data.
+
+        Returns
+        ------------
+        tuple of 2: First is for sample and the second is for label.
+                            First data is binarized if a value is greater than 0, then 1;
+                            otherwise 0.
+        """
+
+        # Read data
+        batch_data_x_ = self.test_data["X"]
+        batch_data_y_ = self.test_data["y"]
+        batch_data_x = ((batch_data_x_ - 127.5)/ 127.5).astype(np.float32)
+        batch_data_y = batch_data_y_.astype(np.int32)
+
+        return batch_data_x , batch_data_y
+
+    def _transform(self, imgs):
+        imgs_ = np.zeros_like(imgs)
+        for i, img in enumerate(imgs):
+            img_ = np.copy(img)
+            
+            # Rotation
+            n = np.random.choice(np.arange(-15, 15))
+            M = cv2.getRotationMatrix2D((32/2, 32/2), n, 1)
+            dst = cv2.warpAffine(img_.transpose(1, 2, 0), M, (32, 32))
+
+            # translation
+            M = np.float32([[1,0,np.random.randint(-2, 2)],
+                            [0,1,np.random.randint(-2, 2)]])
+            dst = cv2.warpAffine(dst, M, (32, 32))
+
+            imgs_[i] = dst.transpose(2, 0, 1)
+        return imgs_
+
+class Separator(object):
+    """Seprate the original samples to labeled samples and unlabeled samples.
+
+    Seprate the original samples to labeled samples and unlabeled samples in such
+    way; the number of labeled samples are selected randomly, it is equal to `l`, 
+    and the others are unlabeled samples.
+
+    Note SVNH dataset is of shape (h, w, c, b) so that Separaor transform to 
+    the shape (b, c, h, w) for the later use efficiently.
+    """
+
+    def __init__(self, l=1000):
+        self.l = l
+
+    def separate_then_save(
+            self,
+            fpath="/home/kzk/.chainer/dataset/pfnet/chainer/svhn/train.mat"):
+        ldata, udata = self._separate(fpath)
+        self._save_ssl_data(fpath, ldata, udata)
+        
+    def _separate(
+            self,
+            fpath="/home/kzk/.chainer/dataset/pfnet/chainer/svhn/train.mat"):
+        
+        data = scipy.io.loadmat(fpath)
+        n = len(data["X"])
+        idxs = np.arange(n)
+        idxs_l = self._sample_indices(np.squeeze(data["y"]))
+        idxs_u = np.asarray(list(set(idxs) - set(idxs_l)))
+
+        ldata = {}
+        udata = {}
+        ldata["X"] = data["X"][idxs_l]
+        ldata["y"] = np.squeeze(data["y"])[idxs_l]
+        udata["X"] = data["X"][idxs_u]
+        udata["y"] = np.squeeze(data["y"])[idxs_u]
+
+        # Shuffle in advance since svhn label is ordered sequencially like 0, 0, ..., 1, 1, ..
+        idx = np.arange(len(ldata["X"]))
+        np.random.shuffle(idx)
+        ldata["X"] = ldata["X"][idx]
+        ldata["y"] = ldata["y"][idx]
+        
+        idx = np.arange(len(udata["X"]))
+        np.random.shuffle(idx)
+        udata["X"] = udata["X"][idx]
+        udata["y"] = udata["y"][idx]
+        
+        return ldata, udata
+
+    def _sample_indices(self, y):
+        classes = set(np.squeeze(y))
+        indicies = []
+        n_for_each_classes = int(1. * self.l / len(classes))
+        for c in classes:
+            indices_for_c = np.where(y==c)[0]
+            indicies += np.random.choice(indices_for_c, n_for_each_classes,
+                                            replace=False).tolist()
+        return indicies
+        
+    def _save_ssl_data(self, fpath, ldata, udata):
+        dpath = os.path.dirname(fpath)
+        fname = os.path.basename(fpath)
+
+        l_fname = "l_{}".format(fname)
+        u_fname = "u_{}".format(fname)
+        
+        ldata_fpath = os.path.join(dpath, l_fname)
+        udata_fpath = os.path.join(dpath, u_fname)
+
+        scipy.io.savemat(ldata_fpath, ldata)
+        scipy.io.savemat(udata_fpath, udata)
+
+class Converter(object):
+    """Convert svhn dataset
+    """
+    def __init__(self, ):
+        self.map_ = {1:0, 
+                     2:1, 
+                     3:2, 
+                     4:3, 
+                     5:4, 
+                     6:5, 
+                     7:6, 
+                     8:7, 
+                     9:8, 
+                     10:9,}
+
+    def convert_then_save(
+            self, 
+            fpath_inp="/home/kzk/.chainer/dataset/pfnet/chainer/svhn/train_32x32.mat", 
+            fpath_out="/home/kzk/.chainer/dataset/pfnet/chainer/svhn/train.mat"):
+        
+        data_ = scipy.io.loadmat(fpath_inp)
+        X = data_["X"].transpose((3, 2, 0, 1))
+        y = [self.map_[x] for x in np.squeeze(data_["y"])]
+        data = {"X": X , "y": y}
+        scipy.io.savemat(fpath_out, data)
