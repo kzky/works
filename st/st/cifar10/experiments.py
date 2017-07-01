1b169f6ee7ab929f6da9fb0fa108f23952c1a663
kzky <rkzfilter@gmail.com>
2017-07-01 18:24:22 +0900

    Add save grad.

--- st/st/cifar10/experiments.py
+++ st/st/cifar10/experiments.py
@@ -173,3 +173,84 @@ class Experiment003(Experiment000):
         self.optimizer = optimizers.Adam(learning_rate)
         self.optimizer.setup(self.model)
         self.optimizer.use_cleargrads()
+
+class Experiment003_Save_Grad(Experiment000):
+    """
+    - ConvPool-CNN-C (Springenberg et al., 2014, Salimans&Kingma (2016))
+    - with large maps
+    """
+    def __init__(self, device=None, learning_rate=1e-3, act=F.relu, n_cls=10, batch_size=64, n_samples=5000):
+        # Settings
+        self.device = device
+        self.act = act
+        self.learning_rate = learning_rate
+        self.n_cls = n_cls
+        self.batch_size = batch_size
+        self.iters = 0
+        self.one_epoch = int(n_samples / batch_size)
+        self.epoch = 0
+        self.basedir = "./{}".format(int(time.time()))
+        if not os.path.exists(basedir):
+            os.makedir(basedir)
+
+        # Loss
+        self.recon_loss = ReconstructionLoss()
+
+        # Model
+        from st.cifar10.cnn_model_003 import Model
+        self.model = Model(device, act)
+        self.model.to_gpu(device) if device is not None else None
+
+        # Optimizer
+        self.optimizer = optimizers.Adam(learning_rate)
+        self.optimizer.setup(self.model)
+        self.optimizer.use_cleargrads()
+        
+    def _train(self, x0, x1, y=None, prefix=""):
+        loss = 0
+
+        # Cross Entropy Loss
+        y_pred0 = self.model(x0)
+        y_pred1 = self.model(x1)
+        if y is not None:
+            loss_ce = F.softmax_cross_entropy(y_pred0, y)
+            loss += loss_ce
+
+        # Stochastic Regularization
+        loss_rec = self.recon_loss(F.softmax(y_pred0), F.softmax(y_pred1))
+        loss += loss_rec
+
+        self.model.cleargrads()
+        loss.backward()
+        self.optimizer.update()
+        
+        self.iters += self.batch_size
+
+        # Save gradients
+        if self.iters % self.one_epoch == 0:
+            self.epoch += 1
+            if y is not None:
+                self.model.cleargrads()
+                loss_ce.backward()
+                self.save_grad("ce")
+            else:
+                self.model.cleargrads()
+                loss_rec.backward()
+                self.save_grad("sr")
+
+    def save_grad(self, grad_type):
+        for name, param in self.model.namedparams():
+            fpath = os.path.join(self.basedir, 
+                                 "{:03}_grad_{}_{}.npz".format(self.epoch, grad_type, name))
+            grad = cuda.to_gpu(param.grad)
+            np.savez(grad, fpath)
+                
+    def train(self, x_l0, x_l1, y_l, x_u0, x_u1):
+        prefix = int(time.time())
+        self._train(x_l0, x_l1, y_l, prefix)
+        self._train(x_u0, x_u1, None, prefix)
+        
+    def test(self, x, y):
+        y_pred = self.model(x, test=True)
+        acc = F.accuracy(y_pred, y)
+        return acc
