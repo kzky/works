d8570c2bb16ee150869dee79ad07f8619d39b435
kzky <rkzfilter@gmail.com>
2017-07-08 23:05:43 +0900

    Correct logic.

--- st/scripts/save_gradient_scale_over_time.py
+++ st/scripts/save_gradient_scale_over_time.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import glob
 import matplotlib.ticker as ticker
 
-def save_grad_scale_over_time(dpath):
+def save_grad_scale_over_time(dpath, filter_="conv_W"):
     # Save gradient as distribution
     fpaths = glob.glob(os.path.join(dpath, "*.npz"))
     fpaths.sort()
@@ -17,28 +17,34 @@ def save_grad_scale_over_time(dpath):
         scale = np.std(data)
 
         basename = os.path.basename(fpath)
-        if "sr__" in basename:
-            scale_over_time_for_sr.append(scale)
-        if "ce__" in basename:
-            scale_over_time_for_sr.append(scale)
+        if filter_ in basename: 
+            if "sr__" in basename:
+                scale_over_time_for_sr.append(scale)
+            if "ce__" in basename:
+                scale_over_time_for_ce.append(scale)
         
     # Save figure of scale over time
-    scale_over_time = np.concatenate(
-        scale_over_time_for_sr, 
-        scale_over_time_for_ce)
     basedir = os.path.dirname(dpath)
     basename = os.path.basename(dpath)
-    save_fpath = os.path.join(basedir, 
-                              "{}_scale_dist_over_time.png".format(basename))
+    save_fpath = os.path.join(
+        basedir, 
+        "{}_{}_scale_dist_over_time.png".format(basename, filter_))
     fig, ax = plt.subplots()
-    ax.plot(scale_over_time)
-    fig.save(save_fpath)
+    ax.set_yscale("log")
+    legend_ce, = ax.plot(scale_over_time_for_ce, 
+                         label="Grad scale for CE loss")
+    legend_sr, = ax.plot(scale_over_time_for_sr, 
+                         label="Grad scale for SR loss")
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles, labels)
+        
+    fig.savefig(save_fpath)
     plt.close(fig)
 
 def main():
-    dpath = "/home/kzk/project/ssl/stochastic_regularization/results/"\
-            "1498905497_conv02"
-    save_grad_scale_over_time(dpath)
+    dpath = "/home/kzky/works/st/experiments/cifar10/1498905497_conv23"
+    filter_ = "bn_gamma"
+    save_grad_scale_over_time(dpath, filter_)
     
 if __name__ == '__main__':
     main()
