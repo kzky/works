6eb99b95892f351c5653a3b533991999f9a46a84
kzky <rkzfilter@gmail.com>
2017-07-13 00:36:07 +0900

    Add comment.

--- st2/experiments/cifar10/exp014.py
+++ st2/experiments/cifar10/exp014.py
@@ -20,6 +20,7 @@ The same script as the `st` module but with nnabla.
 - Entropy Regularization for the outputs before CE loss and SR loss
 - Naive implementation for gradient scaling; basis is grad scale of supervised loss
 - Solver is separated
+- gamma and beta are not scaled
 """
 
 def categorical_error(pred, label):
