8677463a6a7a179836b227c190e26beed3dc4005
kzky <rkzfilter@gmail.com>
2017-07-16 21:14:17 +0900

    Use small maps.

--- stae/stae/cifar10/cnn_ae_model_000.py
+++ stae/stae/cifar10/cnn_ae_model_000.py
@@ -26,32 +26,32 @@ def cnn_ae_model_000(ctx, x, act=F.relu, test=False):
     with nn.parameter_scope("ae"):
         with nn.context_scope(ctx):
             # Convblock0
-            h = conv_unit(x, "conv00", 128, k=3, s=1, p=1, act=act, test=test)
-            h = conv_unit(h, "conv01", 128, k=3, s=1, p=1, act=act, test=test)
-            h = conv_unit(h, "conv02", 128, k=3, s=1, p=1, act=act, test=test)
-            h = conv_unit(h, "conv03", 128, k=4, s=2, p=1, act=act, test=test)  # 32 -> 16
+            h = conv_unit(x, "conv00", 32, k=3, s=1, p=1, act=act, test=test)
+            h = conv_unit(h, "conv01", 32, k=3, s=1, p=1, act=act, test=test)
+            h = conv_unit(h, "conv02", 32, k=3, s=1, p=1, act=act, test=test)
+            h = conv_unit(h, "conv03", 32, k=4, s=2, p=1, act=act, test=test)  # 32 -> 16
             if not test:
                 h = F.dropout(h)
      
             # Convblock 1
-            h = conv_unit(h, "conv10", 256, k=3, s=1, p=1, act=act, test=test)
-            h = conv_unit(h, "conv11", 256, k=3, s=1, p=1, act=act, test=test)
-            h = conv_unit(h, "conv12", 256, k=3, s=1, p=1, act=act, test=test)
-            h = conv_unit(h, "conv13", 256, k=4, s=2, p=1, act=act, test=test) # 16 -> 8
+            h = conv_unit(h, "conv10", 64, k=3, s=1, p=1, act=act, test=test)
+            h = conv_unit(h, "conv11", 64, k=3, s=1, p=1, act=act, test=test)
+            h = conv_unit(h, "conv12", 64, k=3, s=1, p=1, act=act, test=test)
+            h = conv_unit(h, "conv13", 64, k=4, s=2, p=1, act=act, test=test) # 16 -> 8
             if not test:
                 h = F.dropout(h)
      
             # Deconvblock0
-            h = deconv_unit(h, "deconv00", 256, k=4, s=2, p=1, act=act, test=test) # 8 -> 16
-            h = deconv_unit(h, "deconv01", 256, k=3, s=1, p=1, act=act, test=test)
+            h = deconv_unit(h, "deconv00", 64, k=4, s=2, p=1, act=act, test=test) # 8 -> 16
+            h = deconv_unit(h, "deconv01", 64, k=3, s=1, p=1, act=act, test=test)
      
-            h = deconv_unit(h, "deconv02", 256, k=3, s=1, p=1, act=act, test=test)
-            h = deconv_unit(h, "deconv03", 256, k=3, s=1, p=1, act=act, test=test)  
+            h = deconv_unit(h, "deconv02", 64, k=3, s=1, p=1, act=act, test=test)
+            h = deconv_unit(h, "deconv03", 64, k=3, s=1, p=1, act=act, test=test)  
      
             # Deconvblock 1
-            h = deconv_unit(h, "deconv10", 128, k=4, s=2, p=1, act=act, test=test)  # 16 -> 32
-            h = deconv_unit(h, "deconv11", 128, k=3, s=1, p=1, act=act, test=test)
-            h = deconv_unit(h, "deconv12", 128, k=3, s=1, p=1, act=act, test=test)
+            h = deconv_unit(h, "deconv10", 32, k=4, s=2, p=1, act=act, test=test)  # 16 -> 32
+            h = deconv_unit(h, "deconv11", 32, k=3, s=1, p=1, act=act, test=test)
+            h = deconv_unit(h, "deconv12", 32, k=3, s=1, p=1, act=act, test=test)
             h = deconv_unit(h, "deconv13", 3, k=3, s=1, p=1, act=None, test=test)
 
         return h
