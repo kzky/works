c2286cb380e52bed2032650ff8cd1dd4499a9644
kzky <rkzfilter@gmail.com>
2017-07-23 16:47:49 +0900

    Fix.

--- st2/experiments/cifar10/exp024.py
+++ st2/experiments/cifar10/exp024.py
@@ -8,7 +8,7 @@ import numpy as np
 import os
 import time
 import argparse
-from st2.cifar10.cnn_model_018 import resnet_model, ce_loss, sr_loss
+from st2.cifar10.cnn_model_018 import resnet_model, ce_loss, sr_loss, er_loss
 from st2.cifar10.datasets import Cifar10DataReader, Separator
 
 """
