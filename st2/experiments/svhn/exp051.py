ae23baae8c4223b8dbfc09f94923d9504172b8f9
kzky <rkzfilter@gmail.com>
2017-09-09 18:01:19 +0900

    Fix samples.

--- st2/experiments/svhn/exp051.py
+++ st2/experiments/svhn/exp051.py
@@ -37,7 +37,7 @@ def main(args):
     batch_size = args.batch_size
     batch_size_eval = args.batch_size_eval
     n_l_train_data = 1000
-    n_train_data = 50000
+    n_train_data = 73257
     n_cls = 10
     learning_rate = 1. * 1e-3
     n_epoch = 300
