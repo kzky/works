22ddad7317a11d832bd7073fc9b594c458c50483
kzky <rkzfilter@gmail.com>
2017-09-10 18:32:33 +0900

    Fix by making the needed intermediate buffer persistent.

--- st2/experiments/cifar10/exp059.py
+++ st2/experiments/cifar10/exp059.py
@@ -63,6 +63,7 @@ def main(args):
     x_u0 = nn.Variable((batch_size, m, h, w))
     x_u1 = nn.Variable((batch_size, m, h, w))
     pred_x_u0 = cnn_model_003(ctx, "cnn", x_u0)
+    pred_x_u0.persistent = True
     pred_x_u1 = cnn_model_003(ctx, "cnn", x_u1)
     loss_sr = sr_loss(ctx, pred_x_u0, pred_x_u1)
     loss_er0 = er_loss(ctx, pred_x_u0)
