48f6af970d493a9775c4225f8973fad592c79079
kzky <rkzfilter@gmail.com>
2017-10-21 17:31:44 +0900

    Use cifar10's.

--- st2/st2/mnist/datasets.py
+++ st2/st2/mnist/datasets.py
@@ -1,34 +1,44 @@
 import numpy as np
 import os
-from chainer import cuda
 import cv2
 
 class MNISTDataReader(object):
     """DataReader
     """
-    def __init__(self,
-                 l_train_path=\
-                 "/home/kzk/.chainer/dataset/pfnet/chainer/mnist/l_train.npz",
-                 u_train_path=\
-                 "/home/kzk/.chainer/dataset/pfnet/chainer/mnist/u_train.npz", 
-                 test_path="/home/kzk/.chainer/dataset/pfnet/chainer/mnist/test.npz",
-                 batch_size=64,
-                 n_cls=10,
-                 da=False,
-                 shape=False,
+    def __init__(
+            self,
+            l_train_path=\
+            "/home/kzk/.chainer/dataset/pfnet/chainer/mnist/l_cifar-10.npz",
+            u_train_path=\
+            "/home/kzk/.chainer/dataset/pfnet/chainer/mnist/cifar-10.npz", 
+            test_path=\
+            "/home/kzk/.chainer/dataset/pfnet/chainer/mnist/cifar-10.npz",
+            batch_size=64,
+            n_cls=10,
+            da=False,
+            shape=False,
     ):
-            
+        # Load dataset
         self.l_train_data = dict(np.load(l_train_path))
-        self.u_train_data = dict(np.load(u_train_path))
-        self.test_data = dict(np.load(test_path))
+        self.l_train_data = {
+            "train_x":self.l_train_data["x"], 
+            "train_y":self.l_train_data["y"][:, np.newaxis]}
+        _u_train_data = np.load(u_train_path)
+        self.u_train_data = {
+            "train_x":_u_train_data["x"], 
+            "train_y":_u_train_data["y"][:, np.newaxis]}
+        _test_data = np.load(test_path)
+        self.test_data = {
+            "test_x": _test_data["x"], 
+            "test_y": _test_data["y"][:, np.newaxis]}
 
         self._batch_size = batch_size
         self._next_position_l_train = 0
         self._next_position_u_train = 0
 
-        self._n_l_train_data = len(self.l_train_data["x"])
-        self._n_u_train_data = len(self.u_train_data["x"])
-        self._n_test_data = len(self.test_data["x"])
+        self._n_l_train_data = len(self.l_train_data["train_x"])
+        self._n_u_train_data = len(self.u_train_data["train_x"])
+        self._n_test_data = len(self.test_data["test_x"])
         self._n_cls = n_cls
         self._da = da
         self._shape = shape
@@ -41,77 +51,111 @@ class MNISTDataReader(object):
     def reshape(self, x):
         if self._shape:
             bs = x.shape[0]
-            return x.reshape(bs, 1, 28, 28)
+            return x.reshape(bs, 3, 32, 32)
         return x
     
-    def get_l_train_batch(self,):
+    def get_l_train_batch(self, batch_size=None):
         """Return next batch data.
 
         Return next batch data. Once all samples are read, permutate all samples.
         
         Returns
         ------------
-        tuple of 2: First is for sample and the second is for label.
-                            First data is binarized if a value is greater than 0, then 1;
-                            otherwise 0.
+        tuple of 3: ndarray (data), placeholder, ndarray (label)
+
         """
         # Read data
+        if batch_size is None:
+            _batch_size = self._batch_size
+        else:
+            _batch_size = batch_size
+
         beg = self._next_position_l_train
-        end = self._next_position_l_train+self._batch_size
-        batch_data_x_ = self.l_train_data["x"][beg:end, :]
-        batch_data_y_ = self.l_train_data["y"][beg:end]
-        batch_data_x = ((batch_data_x_ - 127.5)/ 127.5).astype(np.float32)
-        if self._da:
-            batch_data_x = self._transform(batch_data_x)
+        end = self._next_position_l_train + _batch_size
+        if end < self._n_l_train_data:
+            batch_data_x_ = self.l_train_data["train_x"][beg:end, :]
+            batch_data_y_ = self.l_train_data["train_y"][beg:end]
+            batch_data_x = (batch_data_x_/ 255.).astype(np.float32)
+        else:
+            bs_s = _batch_size - self._n_l_train_data + beg
+            batch_data_x_ = self.l_train_data["train_x"][beg:end, :]
+            batch_data_y_ = self.l_train_data["train_y"][beg:end]
+            batch_data_x__ = self.l_train_data["train_x"][0:bs_s, :]
+            batch_data_y__ = self.l_train_data["train_y"][0:bs_s]
+            batch_data_x_ = np.concatenate(
+                (batch_data_x_, batch_data_x__))
+            batch_data_y_ = np.concatenate(
+                (batch_data_y_, batch_data_y__))
+            batch_data_x = (batch_data_x_/ 255.).astype(np.float32)
+
+        batch_data_x0 = self._transform(batch_data_x)
         batch_data_y = batch_data_y_.astype(np.int32)
 
         # Reset pointer
-        self._next_position_l_train += self._batch_size
+        self._next_position_l_train += _batch_size
         if self._next_position_l_train >= self._n_l_train_data:
             self._next_position_l_train = 0
 
             # Shuffle
             idx = np.arange(self._n_l_train_data)
             np.random.shuffle(idx)
-            self.l_train_data["x"] = self.l_train_data["x"][idx]
-            self.l_train_data["y"] = self.l_train_data["y"][idx]
+            self.l_train_data["train_x"] = self.l_train_data["train_x"][idx]
+            self.l_train_data["train_y"] = self.l_train_data["train_y"][idx]
 
-        batch_data_x = self.reshape(batch_data_x)
-        return batch_data_x, batch_data_y
+        batch_data_x0 = self.reshape(batch_data_x0)
+        return batch_data_x0, None, batch_data_y
 
-    def get_u_train_batch(self,):
+    def get_u_train_batch(self, batch_size=None):
         """Return next batch data.
 
         Return next batch data. Once all samples are read, permutate all samples.
 
         Returns:
-        tuple of 2: First is for sample and the second is for label.
-                            First data is binarized if a value is greater than 0, then 1;
-                            otherwise 0.
+        tuple of 3: ndarray (data), ndarray (data), ndarray (label)
+
         """
         # Read data
+        if batch_size is None:
+            _batch_size = self._batch_size
+        else:
+            _batch_size = batch_size
+
         beg = self._next_position_u_train
-        end = self._next_position_u_train+self._batch_size
-        batch_data_x_ = self.u_train_data["x"][beg:end, :]
-        batch_data_y_ = self.u_train_data["y"][beg:end]
-        batch_data_x = ((batch_data_x_ - 127.5)/ 127.5).astype(np.float32)
-        if self._da:
-            batch_data_x = self._transform(batch_data_x)
+        end = self._next_position_u_train + _batch_size
+        if end < self._n_u_train_data:
+            batch_data_x_ = self.u_train_data["train_x"][beg:end, :]
+            batch_data_y_ = self.u_train_data["train_y"][beg:end]
+            batch_data_x = (batch_data_x_/ 255.).astype(np.float32)
+        else:
+            bs_s = _batch_size - self._n_u_train_data + beg
+            batch_data_x_ = self.u_train_data["train_x"][beg:end, :]
+            batch_data_y_ = self.u_train_data["train_y"][beg:end]
+            batch_data_x__ = self.u_train_data["train_x"][0:bs_s, :]
+            batch_data_y__ = self.u_train_data["train_y"][0:bs_s]
+            batch_data_x_ = np.concatenate(
+                (batch_data_x_, batch_data_x__))
+            batch_data_y_ = np.concatenate(
+                (batch_data_y_, batch_data_y__))
+            batch_data_x = (batch_data_x_/ 255.).astype(np.float32)
+
+        batch_data_x0 = self._transform(batch_data_x)
+        batch_data_x1 = self._transform(batch_data_x)
         batch_data_y = batch_data_y_.astype(np.int32)
 
         # Reset pointer
-        self._next_position_u_train += self._batch_size
+        self._next_position_u_train += _batch_size
         if self._next_position_u_train >= self._n_u_train_data:
             self._next_position_u_train = 0
 
             # Shuffle
             idx = np.arange(self._n_u_train_data)
             np.random.shuffle(idx)
-            self.u_train_data["x"] = self.u_train_data["x"][idx]
-            self.u_train_data["y"] = self.u_train_data["y"][idx]
-
-        batch_data_x = self.reshape(batch_data_x)
-        return batch_data_x, batch_data_y
+            self.u_train_data["train_x"] = self.u_train_data["train_x"][idx]
+            self.u_train_data["train_y"] = self.u_train_data["train_y"][idx]
+ 
+        batch_data_x0 = self.reshape(batch_data_x0)
+        batch_data_x1 = self.reshape(batch_data_x1)
+        return batch_data_x0, batch_data_x1, batch_data_y
 
     def get_test_batch(self,):
         """Return next batch data.
@@ -124,22 +168,38 @@ class MNISTDataReader(object):
         """
 
         # Read data
-        batch_data_x_ = self.test_data["x"]
-        batch_data_y_ = self.test_data["y"]
-        batch_data_x = ((batch_data_x_ - 127.5)/ 127.5).astype(np.float32)
+        batch_data_x_ = self.test_data["test_x"]
+        batch_data_y_ = self.test_data["test_y"]
+        batch_data_x = (batch_data_x_ / 255.).astype(np.float32)
         batch_data_y = batch_data_y_.astype(np.int32)
 
         batch_data_x = self.reshape(batch_data_x)
         return batch_data_x , batch_data_y
 
     def _transform(self, imgs):
+        bs = imgs.shape[0]
+        imgs = imgs.reshape(bs, 3, 32, 32)
         imgs_ = np.zeros_like(imgs)
         for i, img in enumerate(imgs):
-            # Rotation
+            # random flip
+            if np.random.randint(2):
+                img_ = np.copy(img[:, :, ::-1])
+            else:
+                img_ = np.copy(img)
+
+            # rotation
             n = np.random.choice(np.arange(-15, 15))
-            M = cv2.getRotationMatrix2D((28/2, 28/2), n, 1)
-            dst = cv2.warpAffine(img.reshape(28, 28), M, (28, 28))
-            imgs_[i] = dst.reshape(784)
+            M = cv2.getRotationMatrix2D((32/2, 32/2), n, 1)
+            dst = cv2.warpAffine(img_.transpose(1, 2, 0), M, (32, 32))
+
+            # translation
+            M = np.float32([[1,0,np.random.randint(-2, 2)],
+                            [0,1,np.random.randint(-2, 2)]])
+            dst = cv2.warpAffine(dst, M, (32, 32))
+
+            imgs_[i] = dst.transpose(2, 0, 1)
+
+        imgs_ = imgs_.reshape(bs, 3072)
         return imgs_
 
 class Separator(object):
@@ -150,29 +210,31 @@ class Separator(object):
     and the others are unlabeled samples.
     """
 
-    def __init__(self, l=100):
+    def __init__(self, l=4000):
         self.l = l
 
-    def separate_then_save(self,
-                fpath="/home/kzk/.chainer/dataset/pfnet/chainer/mnist/train.npz"):
+    def separate_then_save(
+            self,
+            fpath="/home/kzk/.chainer/dataset/pfnet/chainer/mnist/cifar-10.npz"):
         ldata, udata = self._separate(fpath)
         self._save_ssl_data(fpath, ldata, udata)
         
-    def _separate(self,
-                 fpath="/home/kzk/.chainer/dataset/pfnet/chainer/mnist/train.npz"):
+    def _separate(
+            self,
+            fpath="/home/kzk/.chainer/dataset/pfnet/chainer/mnist/cifar-10.npz"):
         
         data = np.load(fpath)
-        n = len(data["x"])
+        n = len(data["train_x"])
         idxs = np.arange(n)
-        idxs_l = self._sample_indices(data["y"])
+        idxs_l = self._sample_indices(data["train_y"])
         idxs_u = np.asarray(list(set(idxs) - set(idxs_l)))
 
         ldata = {}
         udata = {}
-        ldata["x"] = data["x"][idxs_l]
-        ldata["y"] = data["y"][idxs_l]
-        udata["x"] = data["x"][idxs_u]
-        udata["y"] = data["y"][idxs_u]
+        ldata["train_x"] = data["train_x"][idxs_l]
+        ldata["train_y"] = data["train_y"][idxs_l]
+        udata["train_x"] = data["train_x"][idxs_u]
+        udata["train_y"] = data["train_y"][idxs_u]
 
         return ldata, udata
 
