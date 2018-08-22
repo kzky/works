2ec87a5c55865b364d830d850d28888a2832f469
kzky <rkzfilter@gmail.com>
2018-08-22 09:48:55 +0900

    Add commands.

--- cvae-research/commands.sh
+++ cvae-research/commands.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-screen -Sdm "exampl-0" bash -c "python train.py -d 2 --train-data-path /data/datasets/celebA/img_align_celeba_png --monitor-path result/example_0"
+screen -Sdm "exampl-0" bash -c "python train.py -d 2 --train-data-path /data/datasets/celebA/img_align_celeba_png --monitor-path result/example_0 --use-deconv"
 
 # Use min/max threshold for logvar
-screen -Sdm "exampl-1" bash -c "python train.py -d 2 --train-data-path /data/datasets/celebA/img_align_celeba_png --monitor-path result/example_1"
+screen -Sdm "exampl-1" bash -c "python train.py -d 2 --train-data-path /data/datasets/celebA/img_align_celeba_png --monitor-path result/example_1 --use-deconv"
 
+# Use upsampling -> convolution 
+screen -Sdm "exampl-2" bash -c "python train.py -d 3 --train-data-path /data/datasets/celebA/img_align_celeba_png --monitor-path result/example_2"
 
 
