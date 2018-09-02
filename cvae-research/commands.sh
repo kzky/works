a5a8cde3518208697709232ea8a740f261ac5315
kzky <rkzfilter@gmail.com>
2018-09-02 09:26:24 +0900

    Add commands.

--- cvae-research/commands.sh
+++ cvae-research/commands.sh
@@ -20,4 +20,9 @@ screen -Sdm "example-5" bash -c "python train.py -d 0 --train-data-path /data/da
 screen -Sdm "example-6" bash -c "python train.py -d 1 --train-data-path /data/datasets/celebA/img_align_celeba_png --monitor-path result/example_6 --lam-fft 1e-2 --use-patch"
 
 
+# Use edge loss (laplacian pyramid)
+screen -Sdm "example-7" bash -c "python train.py -d 0 --train-data-path /data/datasets/celebA/img_align_celeba_png --monitor-path result/example_7"
 
+
+# Use upsampling -> convolution w/ beta1 0.5
+screen -Sdm "example-8" bash -c "python train.py -d 3 --train-data-path /data/datasets/celebA/img_align_celeba_png --monitor-path result/example_8 --beta1 0.5 --beta2 0.9"
