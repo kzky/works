2ea3cb83eeea4089cc665cdf69fd289c613f2ba3
kzky <rkzfilter@gmail.com>
2018-09-02 09:26:42 +0900

    Use static graph again.

--- cvae-research/train.py
+++ cvae-research/train.py
@@ -13,7 +13,7 @@ from functools import reduce
 
 from datasets import data_iterator_celebA
 from args import get_args, save_args
-from models import encoder, decoder, infer, loss_recon, loss_kl, loss_fft
+from models import encoder, decoder, infer, loss_recon, loss_kl, loss_fft, loss_edge
 from helpers import normalize_method, rgb2gray
 
 def train(args):
@@ -27,35 +27,33 @@ def train(args):
     monitor = Monitor(args.monitor_path)
     monitor_recon_loss = MonitorSeries("Reconstruction Loss", monitor, interval=10)
     monitor_kl_loss = MonitorSeries("KL Loss", monitor, interval=10)
+    monitor_edge_loss = MonitorSeries("Edge Loss", monitor, interval=10)
     monitor_time = MonitorTimeElapsed("Training Time", monitor, interval=10)
     monitor_image_origin = MonitorImage("Original Image", monitor, interval=1, num_images=1, 
                                         normalize_method=normalize_method)
     monitor_image_recon = MonitorImage("Reconstruction Image", monitor, interval=1, num_images=1, 
                                        normalize_method=normalize_method)
-
+    # Model
+    x = nn.Variable([args.batch_size, 3, args.ih, args.iw])
+    e = encoder(x, args.maps)
+    z, mu, logvar, var = infer(e)
+    x_recon = decoder(z, args.maps * 32).apply(persistent=True)
+    
+    # Loss
+    recon_loss = loss_recon(x_recon, x).apply(persistent=True)
+    kl_loss = loss_kl(mu, logvar, var).apply(persistent=True)
+    edge_loss = loss_edge(x_recon, x, level=1).apply(persistent=True)
+    loss = recon_loss + kl_loss + edge_loss
+        
     # Solver
     solver = S.Adam(args.lr, args.beta1, args.beta2)
-    
+    solver.set_parameters(nn.get_parameters())
+
     # DataIterator
     di = data_iterator_celebA(args.train_data_path, args.batch_size)
 
-
     # Train loop
     for i in range(args.max_iter):
-        # Model
-        x = nn.Variable([args.batch_size, 3, args.ih, args.iw])
-        e = encoder(x, args.maps)
-        z, mu, logvar, var = infer(e)
-        x_recon = decoder(z, args.maps * 32).apply(persistent=True)
-    
-        # Loss
-        recon_loss = loss_recon(x_recon, x).apply(persistent=True)
-        kl_loss = loss_kl(mu, logvar, var).apply(persistent=True)
-        loss = recon_loss + kl_loss
-    
-        # Set params to solver
-        solver.set_parameters(nn.get_parameters())
-
         # Feed data
         x_data = di.next()[0]
         x.d = x_data
@@ -69,6 +67,7 @@ def train(args):
         # Monitor and save
         monitor_recon_loss.add(i, recon_loss.d)
         monitor_kl_loss.add(i, recon_loss.d)
+        monitor_edge_loss.add(i, edge_loss.d)
         monitor_time.add(i)
         if i % args.save_interval == 0:
             monitor_image_origin.add(i, x.d)
